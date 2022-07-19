cd075378c3e65e5c8c3d7c036782a4e5ad310617
awanthairushan <2019is067@stu.ucsc.cmb.ac.lk>
2022-07-19 12:00:12 +0530

    formatted the code

--- src/components/finding_place/HomeGallery.jsx
+++ src/components/finding_place/HomeGallery.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Carousel, Col, Row} from "react-bootstrap";
+import {Carousel, Col} from "react-bootstrap";
 import HomeBG1 from '../../assets/images/home_image_gallery/HomeBg1.jpg'
 import HomeBG2 from '../../assets/images/home_image_gallery/HomeBg2.jpg'
 import HomeBG3 from '../../assets/images/home_image_gallery/HomeBg3.jpg'
@@ -10,59 +10,57 @@ import HomeBG7 from '../../assets/images/home_image_gallery/HomeBg7.jpg'
 
 function HomeGallery() {
     return (
-        <Col>
-            <Carousel indicators={false} controls={false} className='home-gallery p-0 m-0' interval={6000}>
-                <Carousel.Item className='gallery-carousal'>
-                    <img
-                        className="d-block border border-dark gallery-image "
-                        src={HomeBG1}
-                        alt="First slide"
-                    />
-                </Carousel.Item>
-                <Carousel.Item className='gallery-carousal'>
-                    <img
-                        className="d-block border border-dark gallery-image"
-                        src={HomeBG2}
-                        alt="Second slide"
-                    />
-                </Carousel.Item>
-                <Carousel.Item className='gallery-carousal'>
-                    <img
-                        className="d-block border border-dark gallery-image"
-                        src={HomeBG3}
-                        alt="Third slide"
-                    />
-                </Carousel.Item>
-                <Carousel.Item className='gallery-carousal'>
-                    <img
-                        className="d-block border border-dark gallery-image"
-                        src={HomeBG4}
-                        alt="Third slide"
-                    />
-                </Carousel.Item>
-                <Carousel.Item className='gallery-carousal'>
-                    <img
-                        className="d-block border border-dark gallery-image"
-                        src={HomeBG5}
-                        alt="Third slide"
-                    />
-                </Carousel.Item>
-                <Carousel.Item className='gallery-carousal'>
-                    <img
-                        className="d-block border border-dark gallery-image"
-                        src={HomeBG6}
-                        alt="Third slide"
-                    />
-                </Carousel.Item>
-                <Carousel.Item className='gallery-carousal'>
-                    <img
-                        className="d-block border border-dark gallery-image"
-                        src={HomeBG7}
-                        alt="Third slide"
-                    />
-                </Carousel.Item>
-            </Carousel>
-        </Col>
+        <Carousel indicators={false} controls={false} className='home-gallery p-0 m-0' interval={6000}>
+            <Carousel.Item className='gallery-carousal'>
+                <img
+                    className="d-block border border-dark gallery-image "
+                    src={HomeBG1}
+                    alt="First slide"
+                />
+            </Carousel.Item>
+            <Carousel.Item className='gallery-carousal'>
+                <img
+                    className="d-block border border-dark gallery-image"
+                    src={HomeBG2}
+                    alt="Second slide"
+                />
+            </Carousel.Item>
+            <Carousel.Item className='gallery-carousal'>
+                <img
+                    className="d-block border border-dark gallery-image"
+                    src={HomeBG3}
+                    alt="Third slide"
+                />
+            </Carousel.Item>
+            <Carousel.Item className='gallery-carousal'>
+                <img
+                    className="d-block border border-dark gallery-image"
+                    src={HomeBG4}
+                    alt="Third slide"
+                />
+            </Carousel.Item>
+            <Carousel.Item className='gallery-carousal'>
+                <img
+                    className="d-block border border-dark gallery-image"
+                    src={HomeBG5}
+                    alt="Third slide"
+                />
+            </Carousel.Item>
+            <Carousel.Item className='gallery-carousal'>
+                <img
+                    className="d-block border border-dark gallery-image"
+                    src={HomeBG6}
+                    alt="Third slide"
+                />
+            </Carousel.Item>
+            <Carousel.Item className='gallery-carousal'>
+                <img
+                    className="d-block border border-dark gallery-image"
+                    src={HomeBG7}
+                    alt="Third slide"
+                />
+            </Carousel.Item>
+        </Carousel>
     );
 }
 
