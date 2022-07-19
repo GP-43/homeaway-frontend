f0a29ba715133b2d700f30d00b1eb6bc7b9f6829
Thushan Dananjana <62166556+Thushan98@users.noreply.github.com>
2022-07-20 01:37:54 +0530

    some modifications done
    Some modification done. unwanted files removed. Try to shorten the code.

--- src/components/occupant_footer/OccupantFooter.jsx
+++ src/components/occupant_footer/OccupantFooter.jsx
@@ -8,22 +8,22 @@ import { GrMail } from "react-icons/gr";
 function OccupantFooter() {
     return (
         
-        <Card className="bg-dark text-center text-light" style={{ width: '100%' }}>
+        <Card className="bg-dark text-center text-light occu-footer">
             <Card.Header as="h3" className="bg-dark">HomeAway</Card.Header>
             <Card.Body className="bg-dark">
                 <Card.Title as="h6" className="bg-dark">HomeAWay is a website where you can find a place with facilities to gather and work.</Card.Title>
                 <Card.Text className="bg-dark">
-                    <FiTwitter className="bg-dark" style={{ width: '45px', height:'37px' }}/>{' '}
-                    <AiOutlineFacebook className="bg-dark" style={{ width: '45px', height:'37px' }}/>{' '}
-                    <AiOutlineLinkedin className="bg-dark" style={{ width: '45px', height:'37px' }}/>{' '}
-                    <FiPhoneCall className="bg-dark" style={{ width: '45px', height:'37px' }}/>{' '}
-                    <GrMail className="bg-dark" style={{ width: '45px', height:'37px' }}/>
+                    <FiTwitter className="bg-dark footer-icon"/>{' '}
+                    <AiOutlineFacebook className="bg-dark footer-icon"/>{' '}
+                    <AiOutlineLinkedin className="bg-dark footer-icon"/>{' '}
+                    <FiPhoneCall className="bg-dark footer-icon"/>{' '}
+                    <GrMail className="bg-dark footer-icon"/>
                 </Card.Text>
                 <Button variant="outline-light dark ">Home</Button>{' '}
                 <Button variant="outline-light dark ">Booking</Button>{' '}
                 <Button variant="outline-light dark ">Terms</Button>
             </Card.Body>
-            <Card.Footer>&copy;{new Date().getFullYear()} HomeAway HomeAway - Alright Resesrved</Card.Footer>
+            <Card.Footer>&copy;{new Date().getFullYear()} HomeAway HomeAway - Allright Resesrved</Card.Footer>
         </Card>
     );
 }
