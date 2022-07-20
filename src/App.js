140f4ff7214ac35ae011e6762352896b29b6d277
Semini48 <84171007+Semini48@users.noreply.github.com>
2022-07-21 00:01:07 +0530

    place card done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.scss'
 import {Routes, Route} from 'react-router-dom';
 import UserHome from './views/pages/UserHome'
+import PlacesCard from './views/PlacesCard';
 
 function App() {
     return (
         <div>
             <Routes>
                 <Route path='/' element={<UserHome/>}/>
+                <Route path='/placecard' element={<PlacesCard/>}/>
             </Routes>
         </div>
     );
---
+++ src/components/PlaceCard.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import { Card } from "react-bootstrap";
+import place from "../assets/images/places_image_gallery/place1.jpg";
+import { FaUserFriends } from "react-icons/fa";
+import { FaStar } from "react-icons/fa";
+import { FaRegHeart } from "react-icons/fa";
+import { FaHeart } from "react-icons/fa";
+
+function PlaceCard() {
+
+  const [show, setShow] = useState(true);
+
+  return (
+    <Card className="card">
+      <img
+        className="d-block gallery-image place-image"
+        src={place}
+        alt="plae card"
+      />
+      {show ? (
+        <button className="button" onClick={() => setShow(false)}>
+          <FaRegHeart className="heart-icon" />
+        </button>
+      ) : (
+        <button className="button" onClick={() => setShow(true)}>
+          <FaHeart className="heart-icon" />
+        </button>
+      )}
+
+      <Card.Body className="text-box">
+        <Card.Body className="text">
+          <Card.Title className="title">Meeting room</Card.Title>
+          <Card.Text className="city">Colombo</Card.Text>
+          <Card.Text className="price">LKR 2500</Card.Text>
+          <Card.Body className="last-row">
+            <Card.Body className="quantity-box">
+              <FaUserFriends className="people-icon" />
+              <Card.Text className="quantity">8</Card.Text>
+            </Card.Body>
+            <Card.Body className="rating-box">
+              <FaStar className="rating-icon" />
+              <Card.Text className="rating">4.0</Card.Text>
+            </Card.Body>
+          </Card.Body>
+        </Card.Body>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default PlaceCard;
