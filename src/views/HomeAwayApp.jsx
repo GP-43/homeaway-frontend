f1f6b231d94a3cadee96bcd0e2454e3d43701431
Tharaki-99 <dilshanitharaki@gmail.com>
2022-07-21 19:59:44 +0530

    Revert "updated navbar"
    This reverts commit 7f5d462bce261fd235b7e117d4c1c484b863f041.

--- src/views/HomeAwayApp.jsx
+++ src/views/HomeAwayApp.jsx
@@ -1,25 +1,17 @@
 import React from "react";
-import { Row, Col, Container } from "react-bootstrap";
+import {Row,Col} from "react-bootstrap";
 import OccupantFooter from "../components/occupent_footer/OccupantFooter";
-import NavBar from "../components/navbar/NavBar";
-import { Outlet } from 'react-router-dom';
+import {Outlet} from 'react-router-dom';
 
 function HomeAwayApp() {
     return (
         <>
-            <Container>
-                <Row className="mx-0">
-                    <Col className="px-0">
-                        <NavBar />
-                    </Col>
-                </Row>
-            </Container>
-            <Outlet />
-            <Row className="mx-0">
-                <Col className="px-0">
-                    <OccupantFooter />
-                </Col>
-            </Row>
+        <Outlet/>
+        <Row className="mx-0">
+            <Col className="px-0">
+                <OccupantFooter/>
+            </Col>
+        </Row>
         </>
     );
 }
