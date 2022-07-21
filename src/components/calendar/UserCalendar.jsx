85723f50ee3b606b2b132380d8fafc85f2180646
Thushan Dananjana <62166556+Thushan98@users.noreply.github.com>
2022-07-21 13:46:13 +0530

    commit
    css halfly done

--- src/components/calendar/UserCalendar.jsx
+++ src/components/calendar/UserCalendar.jsx
@@ -1,23 +1,24 @@
 import 'react-calendar/dist/Calendar.css'
 import Calendar from 'react-calendar'
-import {React, useState} from 'react';
+import React, { useState } from 'react';
+import Card from 'react-bootstrap/Card';
 import '../../assets/styles/partials/_calendar.scss';
 
 function UserCalendar() {
  const [date, setDate] = useState(new Date())
 
-
-
-return (
-  <div className="app">
+  return (
+    <Card className="card">
+      <div className="app">
     
-    <div className="calendar-container">
-      <Calendar onChange={setDate} value={date}/>
-    </div>
-    <div className="text-center">
-      Selected date: {date.toDateString()}
-    </div>
-  </div>
+        <div className="calendar-container">
+          <Calendar className="bg-light" onChange={setDate} value={date}/>
+        </div>
+        <div className="text-center">
+          Selected date: {date.toDateString()}
+        </div>
+      </div>
+    </Card>
   );
 
 }
