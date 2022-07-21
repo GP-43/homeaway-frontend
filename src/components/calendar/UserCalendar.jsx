029501f30914aa252fe20ae727ae7a4b45514828
Thushan Dananjana <62166556+Thushan98@users.noreply.github.com>
2022-07-21 15:24:28 +0530

    Calendar Done
    Calendar Done

--- src/components/calendar/UserCalendar.jsx
+++ src/components/calendar/UserCalendar.jsx
@@ -1,7 +1,6 @@
 import 'react-calendar/dist/Calendar.css'
 import Calendar from 'react-calendar'
 import React, { useState } from 'react';
-//import Card from 'react-bootstrap/Card';
 import '../../assets/styles/partials/_calendar.scss';
 
 function UserCalendar() {
@@ -10,17 +9,15 @@ function UserCalendar() {
   return (
     <div className="card">
       <div className="app">
-    
         <div className="calendar-container">
           <Calendar className="bg-light" onChange={setDate} value={date}/>
         </div>
         <div className="text-center">
-          Selected date: {date.toDateString()}
+          {/*Selected date: {date.toDateString()}*/}
         </div>
       </div>
     </div>
   );
-
 }
 
 export default UserCalendar;
