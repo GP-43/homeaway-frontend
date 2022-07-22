fbfcd1a7f6d71f4b45a33cc523f6f5387f41e1af
Thushan Dananjana <62166556+Thushan98@users.noreply.github.com>
2022-07-23 00:02:09 +0530

    Filter section initiated
    initiated filter section. 1st commit

---
+++ src/components/finding_place/FilterSection.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {Col, Row} from "react-bootstrap";
+
+function FilterSection() {
+    return (
+        <Row>
+            hello
+        </Row>
+    );
+}
+
+export default FilterSection;
