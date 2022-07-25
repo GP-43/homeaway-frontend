b2949242867c87c612d5027794a1ac5314c0617e
Tharindu69 <2019cs169@stu.ucsc.cmb.ac.lk>
2022-07-25 11:58:58 +0530

    Thushan reviewed

--- src/components/shedule_section/SheduleCard.jsx
+++ src/components/shedule_section/SheduleCard.jsx
@@ -4,7 +4,7 @@ import { Card, Col, Container, Row } from "react-bootstrap";
 function SheduleCard(props) {
     return (
 
-        <Container>
+        <Container className="ps-0">
             <Card className="schedule-card ps-4 pb-2 pe-2 pt-2  mb-3" >
 
                 <Col xs={7} className="m-auto ">
--- src/components/shedule_section/SheduleSection.jsx
+++ src/components/shedule_section/SheduleSection.jsx
@@ -16,7 +16,7 @@ function SheduleSection() {
         <Container className="mt-1">
             <div className="schedule-section-box">
                 <div className="schedule-section">
-                    <h3 className="schedule-section-title">Schedule</h3>
+                    <h5 className="schedule-section-title">Schedule</h5>
                     {schedule_data && schedule_data.map(i => <SheduleCard Title={i.title} Place={i.place} From={i.from} To={i.to} />)}
                 </div>
             </div>
