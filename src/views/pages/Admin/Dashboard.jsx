be67aa571beb5e3ca74fe9fa391e302d8b4f32e9
awanthairushan <2019is067@stu.ucsc.cmb.ac.lk>
2022-07-25 18:42:38 +0530

    create admin page

---
+++ src/views/pages/Admin/Dashboard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {Row, Col, Container} from "react-bootstrap";
+
+function Dashboard() {
+    return (
+        <Container>
+            <Row className="mx-0">
+                <Col className="px-0">
+                    dashboard components
+                </Col>
+            </Row>
+        </Container>
+    );
+}
+
+export default Dashboard;
