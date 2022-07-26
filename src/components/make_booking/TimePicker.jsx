052c554017aaa71bfa7f1936d7c0172ae25fb3ad
Thushan Dananjana <62166556+Thushan98@users.noreply.github.com>
2022-07-26 19:48:59 +0530

    created time picker branch
    created time picker branch

---
+++ src/components/make_booking/TimePicker.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {Row, Col, Container} from "react-bootstrap";
+
+function TimePicker() {
+    return (
+        <Container>
+            <Row className="mx-0">
+                <Col className="px-0">
+                    Time picking
+                </Col>
+            </Row>
+        </Container>
+    );
+}
+
+export default TimePicker;
