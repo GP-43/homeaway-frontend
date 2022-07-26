51b1526ee250765856fb962e701176e12cb82892
Thushan Dananjana <62166556+Thushan98@users.noreply.github.com>
2022-07-26 23:07:23 +0530

    partial done
    adding css

--- src/components/make_booking/TimePicker.jsx
+++ src/components/make_booking/TimePicker.jsx
@@ -1,14 +1,28 @@
 import React from "react";
-import {Row, Col, Container} from "react-bootstrap";
+import {Row, Col, Container, Form, Button} from "react-bootstrap";
 
 function TimePicker() {
     return (
-        <Container>
-            <Row className="mx-0">
-                <Col className="px-0">
-                    Time picking
-                </Col>
-            </Row>
+        <Container className="bg-white">
+            <Col className="mx-0 bg-white">
+                <Form>
+                <Row className="time-grid-container bg-white">
+                    <div className="time-grid-item">
+                        <input type="text" id="hours" className="time-selection"></input> : 
+                    </div>
+                    
+                    <div className="time-grid-item">
+                        <input type="text" id="minutes" className="time-selection"></input>
+                    </div>
+                    <div className="time-grid-item">
+                        <Button className="time-button">AM</Button>
+                        <Button className="time-button">PM</Button>
+                    </div>    
+                </Row>
+                <h6>Minutes</h6>
+                    <h6>Hours</h6>
+                </Form>
+            </Col>
         </Container>
     );
 }
--- src/views/pages/PlaceDescription.jsx
+++ src/views/pages/PlaceDescription.jsx
@@ -2,17 +2,20 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import ImageGallery from '../../components/imagegallery/ImageGallery';
 import TimePicker from '../../components/make_booking/TimePicker';
+//import DateSelection from '../../components/make_booking/DateSelection';
 
 function PlaceDescription(){
   return (
     <Container >
         <Row className='mx-0 mt-2'>
-            <Col className='image-gallery-container'>
+            <Col className='image-gallery-container bg-dark'>
                 <ImageGallery/>
             </Col>
-            <Col>
+            <Col className='place-description-right ms-5 bg-info'>
                 <Row>Owner Details</Row>
-                <Row><TimePicker/></Row>
+                <Row className='mx-0 mt-2'>
+                    <TimePicker/>
+                </Row>
             </Col>
         </Row>
     </Container>
