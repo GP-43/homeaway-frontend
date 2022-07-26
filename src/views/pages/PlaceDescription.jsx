656c4072ffbe6a95e99c1edd757555a7823dc2cf
Thushan Dananjana <62166556+Thushan98@users.noreply.github.com>
2022-07-26 21:07:41 +0530

    Place description modified
    changed container class name

--- src/views/pages/PlaceDescription.jsx
+++ src/views/pages/PlaceDescription.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import ImageGallery from '../../components/imagegallery/ImageGallery';
+import TimePicker from '../../components/make_booking/TimePicker';
 
 function PlaceDescription(){
   return (
-    <Container className='image-gallery-container'>
+    <Container >
         <Row className='mx-0 mt-2'>
-            <Col>
+            <Col className='image-gallery-container'>
                 <ImageGallery/>
+            </Col>
+            <Col>
+                <Row>Owner Details</Row>
+                <Row><TimePicker/></Row>
             </Col>
         </Row>
     </Container>
