f458f4fb2d541686218f2d397a5a720f27a4e1ad
Thushan Dananjana <62166556+Thushan98@users.noreply.github.com>
2022-07-28 02:25:52 +0530

    CSS changes are done
    add more css

--- src/components/make_booking/TimePickers.jsx
+++ src/components/make_booking/TimePickers.jsx
@@ -15,14 +15,14 @@ function TimePickers() {
                     <span className="time-input-span bg-white px-0"><p className="time-input-span-text bg-white">Hours</p></span>
                         
                     <div className="time-dots px-3 mx-3"><h4 className="time-dots-h4 bg-white py-2 px-0">:</h4></div> 
-                      
+
                     <div className="time-box-1 m-0 px-0">
                         <input type="text" id="minutes" className="time-input bg-white" placeholder="00"></input>
                     </div>
                     <span className="time-input-span bg-white p-0"><p className="time-input-span-text bg-white">Minutes</p></span>
                        
                     <div className="time-button-div px-3 mx-3">
-                        <Button className="time-button am" id="am"><p className="button-name">AM</p></Button>
+                        <Button className="time-button am" id="am" active><p className="button-name">AM</p></Button>
                         <Button className="time-button pm" id="pm"><p className="button-name">PM</p></Button>
                     </div>
                    
