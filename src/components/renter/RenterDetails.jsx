556890f60959ae531b7119f055eedb304eba9933
Semini48 <84171007+Semini48@users.noreply.github.com>
2022-07-27 14:09:05 +0530

    errors done

--- src/components/renter/RenterDetails.jsx
+++ src/components/renter/RenterDetails.jsx
@@ -5,32 +5,28 @@ import { FaStar } from "react-icons/fa";
 
 function RenterDetails() {
   return (
-    <Container>
-      <Row>
-        <Col>
-          <Card className="renter-card">
-            <Card.Body className="renter-body">
-              <Row className="md-2 renter-row">
-                <Col className="renter-col text-center">
-                  <img className="renter-image" src={girl1} alt="girl1" />
-                  <Col className="renter-name">Ms.Senulya Wijewardhane</Col>
-                  <Col className="renter-rating">
-                    <FaStar className="rating-icon" />
-                    &nbsp;4.5
-                  </Col>
+    <Row>
+      <Col>
+        <Card className="renter-card">
+          <Card.Body className="renter-body">
+            <Row className="md-2 renter-row">
+              <Col className="renter-col text-center">
+                <img className="renter-image" src={girl1} alt="girl1" />
+                <Col className="renter-name">Ms.Senulya Wijewardhane</Col>
+                <Col className="renter-rating">
+                  <FaStar className="rating-icon" />
+                  &nbsp;4.5
                 </Col>
-                <Col className="renter-col text-center">
-                  <Col className="renter-no">+94 71 *** *****</Col>
-                  <Col className="renter-message">
-                    Message owner
-                  </Col>
-                </Col>
-              </Row>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
+              </Col>
+              <Col className="renter-col text-center">
+                <Col className="renter-no">+94 71 *** *****</Col>
+                <Col className="renter-message">Message owner</Col>
+              </Col>
+            </Row>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
   );
 }
 
