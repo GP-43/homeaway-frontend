b33972d9f767ee21aa47d6a2ed10e978b1b339e9
Semini48 <84171007+Semini48@users.noreply.github.com>
2022-07-28 07:53:21 +0530

    daily transaction partially done

---
+++ src/components/admin/DailyTransactions.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {Bar} from "react-chartjs-2";
+import {Col} from "react-bootstrap";
+
+function DailyTransactions() {
+  return( 
+  <Col>
+  {/* <Bar data={{labels:['Red', 'Blue', 'Yellow']}} height={400} width={600}/> */}
+    </Col>
+  );
+}
+
+export default DailyTransactions;
--- src/views/pages/Admin/Dashboard.jsx
+++ src/views/pages/Admin/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {Row, Col, Container} from "react-bootstrap";
+import DailyTransactions from "../../../components/admin/DailyTransactions";
 import HeaderCard from "../../../components/admin/HeaderCard";
 
 function Dashboard() {
@@ -8,6 +9,11 @@ function Dashboard() {
             <Row className="mx-0">
                 <Col className="px-0">
                     <HeaderCard/>
+                </Col>
+            </Row>
+            <Row className="mx-0">
+                <Col className="px-0">
+                    <DailyTransactions/>
                 </Col>
             </Row>
         </Container>
