dc82d5fc86cc219e2469dfc2e024716cbb89c5cd
Tharaki-99 <dilshanitharaki@gmail.com>
2022-07-29 12:23:55 +0530

    partially done- detail cards

--- src/components/admin/SingleUser.jsx
+++ src/components/admin/SingleUser.jsx
@@ -11,11 +11,11 @@ function SingleUser(props) {
                     <Row className='user-row'>
                         <Col className='user-col'>
                             <img
-                                className="profile-image p-0 ms-5 mb-2"
+                                className="profile-image p-0 ms-4 mb-2"
                                 src={props.Src}
                             />
                         </Col >
-                        <Col className='text-col'>
+                        <Col xs={7} className='text-col'>
                             <Row className="user-name ">{props.Name}</Row>
                             <Row className="user-location pb-4">{props.Location}</Row>
                         </Col>
