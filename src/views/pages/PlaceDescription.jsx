bf246d4c93a47534b1692a1c5ca3daa2663dbe2b
Thushan Dananjana <62166556+Thushan98@users.noreply.github.com>
2022-07-30 11:28:48 +0530

    changing structure

--- src/views/pages/PlaceDescription.jsx
+++ src/views/pages/PlaceDescription.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import ImageGallery from '../../components/imagegallery/ImageGallery';
-import DatePickers from '../../components/make_booking/DatePickers';
-import TimePickers from '../../components/make_booking/TimePickers';
+import MakeBooking from '../../components/make_booking/MakeBooking';
 
 function PlaceDescription(){
   return (
@@ -13,34 +12,7 @@ function PlaceDescription(){
             </Col>
             <Col className='place-description-right ms-5'>
                 <Row>Owner Details section</Row>
-                <Row className='make-booking-card1 mx-0 mt-2 bg-primary d-flex'>
-                    <Col className="booking-col" xs lg="2">
-                        <div className='d-flex m-1 pb-1'>
-                            <b className='text'>Date:</b>
-                            <DatePickers className="date-component"/>
-                        </div>
-                        <div className='d-flex m-1 pb-1'>
-                            <b className='text-time'>Time:</b>
-                            <TimePickers />
-                        </div>
-                        <div className='mx-5 ps-2'>
-                            <h1 className='text-rent-fee'><b>LKR 12,500</b></h1>
-                        </div>
-                    </Col>
-                    <Col className="booking-col" xs lg="2">
-                        <div className='d-flex m-1 pb-1'>
-                            <b className='text'>to:</b>
-                            <DatePickers className="date-component"/>
-                        </div>
-                        <div className='d-flex m-1 pb-1'>
-                            <b className='text-time'>to:</b>
-                            <TimePickers/>
-                        </div>
-                        <div className='ms-5 ps-5'>
-                            <Button variant="dark button-pay-now">Pay Now</Button>
-                        </div>
-                    </Col>
-                </Row>
+                <Row><MakeBooking/></Row>
             </Col>
         </Row>
     </Container>
