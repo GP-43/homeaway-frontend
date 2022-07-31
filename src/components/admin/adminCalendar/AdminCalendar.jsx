b5be84c284aa96b6246c52e17427ef038aa15d91
Thushan Dananjana <62166556+Thushan98@users.noreply.github.com>
2022-07-31 10:33:48 +0530

    css applying

--- src/components/admin/adminCalendar/AdminCalendar.jsx
+++ src/components/admin/adminCalendar/AdminCalendar.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import {Row,Col} from 'react-bootstrap';
+import Calendar from 'react-calendar';
+import '../../../assets/styles/partials/admin/_admin-calendar.scss'
 
 function AdminCalendar() {
+    const [date, setDate] = useState(new Date());
     return (
         <Row>
-
+            
+            <div>
+                <Calendar onChange={setDate} value={date}/>
+            </div>
+            <div className="text-center">
+                Selected date: {date.toDateString()}
+            </div>
         </Row>
     );
 }
