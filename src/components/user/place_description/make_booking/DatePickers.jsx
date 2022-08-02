a4b8c66845ef5443aac8a47941086733fb0ad895
Thushan Dananjana <62166556+Thushan98@users.noreply.github.com>
2022-08-03 00:01:22 +0530

    making icon clickable

--- src/components/user/place_description/make_booking/DatePickers.jsx
+++ src/components/user/place_description/make_booking/DatePickers.jsx
@@ -6,11 +6,15 @@ import "react-datepicker/dist/react-datepicker.css";
 
 function DatePickers() {
     const [startDate, setStartDate] = useState(new Date());
+    const showCalendar = () => {
+        setShowCalendar(true);
+        setFindClickedDatePicker(1)
+    }
     return (
         
             
                 <Row className="date-card bg-white p-1"> 
-                    <Col className="date-icon-col p-0 mx-2 bg-white"><BsFillCalendarEventFill className="date-icon m-2 bg-white"/></Col>
+                    <Col className="date-icon-col p-0 mx-2 bg-white"><BsFillCalendarEventFill className="date-icon m-2 bg-white" onClick={showCalendar}></Col>
                     <Col className="date-datepicker-col px-0 me-2 bg-white">
                         <DatePicker className="datePicker m-2 bg-white" selected={startDate} onChange={(date) => setStartDate(date)}/>
                     </Col>
