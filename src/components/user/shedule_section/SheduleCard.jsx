d4228751d7f960a284a9769b547e98bc97cf93ef
awanthairushan <2019is067@stu.ucsc.cmb.ac.lk>
2022-08-03 21:06:19 +0530

    removed unwanted container

--- src/components/user/shedule_section/SheduleCard.jsx
+++ src/components/user/shedule_section/SheduleCard.jsx
@@ -1,25 +1,22 @@
 import React from "react";
-import { Card, Col, Container, Row } from "react-bootstrap";
+import {Card, Col, Row} from "react-bootstrap";
 
 function SheduleCard(props) {
     return (
+        <Card className="schedule-card ps-4 pb-2 pe-2 pt-2  mb-3">
 
-        <Container className="ps-0">
-            <Card className="schedule-card ps-4 pb-2 pe-2 pt-2  mb-3" >
-
-                <Col xs={7} className="m-auto ">
-                    <Row className="m-auto schedule-card-text">
-                        {props.Title}
-                    </Row>
-                    <Row className="m-auto schedule-card-text">
-                        {props.Place}
-                    </Row>
-                </Col>
-                <Col xs={5} className="m-auto schedule-card-text">
-                    {props.From} to {props.To}
-                </Col>
-            </Card>
-        </Container>
+            <Col xs={7} className="m-auto ">
+                <Row className="m-auto schedule-card-text">
+                    {props.Title}
+                </Row>
+                <Row className="m-auto schedule-card-text">
+                    {props.Place}
+                </Row>
+            </Col>
+            <Col xs={5} className="m-auto schedule-card-text">
+                {props.From} to {props.To}
+            </Col>
+        </Card>
 
     );
 }
