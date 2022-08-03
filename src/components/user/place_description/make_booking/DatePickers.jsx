101ab13b2f6a5b89864d68f5787d4b4aa48d1eb0
Thushan Dananjana <62166556+Thushan98@users.noreply.github.com>
2022-08-03 11:59:29 +0530

    css changes done

--- src/components/user/place_description/make_booking/DatePickers.jsx
+++ src/components/user/place_description/make_booking/DatePickers.jsx
@@ -5,18 +5,22 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 function DatePickers() {
-    // const [startDate, setStartDate] = useState(new Date());
-    // const showCalendar = () => {
-    //     setShowCalendar(true);
-    //     setFindClickedDatePicker(1)
-    // }
+    const [startDate, setStartDate] = useState(new Date());
+
     return (
-        
-            
+
+
                 <Row className="date-card bg-white p-1"> 
-                    <Col className="date-icon-col p-0 mx-2 bg-white"><BsFillCalendarEventFill className="date-icon m-2 bg-white"/></Col>
+                    <Col className="date-icon-col p-0 mx-2 bg-white">
+                        <BsFillCalendarEventFill
+                            className="date-icon m-2 bg-white"
+                        /></Col>
                     <Col className="date-datepicker-col px-0 me-2 bg-white">
-                        <DatePicker className="datePicker m-2 bg-white"/>
+                        <DatePicker
+                            className="datePicker m-2 bg-white"
+                            selected={startDate}
+                            onChange={(date) => setStartDate(date)}
+                        />
                     </Col>
                 </Row>
                      
