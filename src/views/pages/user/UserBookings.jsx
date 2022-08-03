7d4866bf43e3357e6087666401a8e297af4e7767
Thushan Dananjana <62166556+Thushan98@users.noreply.github.com>
2022-08-03 12:14:58 +0530

    tab view done

--- src/views/pages/user/UserBookings.jsx
+++ src/views/pages/user/UserBookings.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import SheduleSection from "../../../components/user/shedule_section/SheduleSection";
 import MyBookingSection from "../../../components/user/my_bookings/MyBookingSection";
-import { Col, Row, Container} from "react-bootstrap";
+import {Col, Row, Container} from "react-bootstrap";
 import Calendar from 'react-calendar';
 import '../../../assets/styles/partials/user/_calendar.scss';
 
@@ -10,13 +10,19 @@ function UserBookings(props) {
     return (
         <Container>
             <Row className='mx-0 mt-2'>
-                <Col lg = {8} className='px-0'>
-                  <MyBookingSection  />                    
+                <Col lg={8} className='px-0'>
+                    <MyBookingSection/>
                 </Col>
-            
-                <Col lg = {4}>
-                    <Calendar className="ms-4 mb-4"/>
-                    <SheduleSection  />
+
+                <Col lg={4}>
+                    <Row className='mx-0'>
+                        <Col className='px-0'>
+                            <Calendar className="ms-4 mb-4" lg={12} md={6}/>
+                        </Col>
+                        <Col className='px-0' lg={12} md={6}>
+                            <SheduleSection/>
+                        </Col>
+                    </Row>
                 </Col>
             </Row>
         </Container>
