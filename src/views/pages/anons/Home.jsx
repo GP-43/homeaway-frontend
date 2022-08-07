bf5936374fb002ddb2df3c9a1db5e054c1f0f7d5
Tharindu69 <2019cs169@stu.ucsc.cmb.ac.lk>
2022-08-07 23:10:23 +0530

    mama hithanawa dan nam harii kiyala,

--- src/views/pages/anons/Home.jsx
+++ src/views/pages/anons/Home.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import {Col, Container, Row} from "react-bootstrap";
 import AboutSection from '../../../components/anons/aboutsection/AboutSection';
+import ThingsYouNeedSection  from "../../../components/anons/things_you_need_section/ThingsYouNeedSection"; 
 
 function Home() {
     return (
         <Container>
             <Row className='mx-0' xs={12}>
-                <Col className='px-0'>
+                <Col className='px-0 vh-100'>
                     Hello section goes here
                 </Col>
-                <Col className='px-0' xs={12}>
-                    Things you need section goes here
+                <Col className='px-0 vh-100 d-flex' xs={12}>
+                    <ThingsYouNeedSection/>
                 </Col>
                 <Col className='px-0' xs={12}>
                     Vacation plane section goes here
