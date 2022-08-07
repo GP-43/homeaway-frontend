9f6cc2204094e093e173913f9d7eb40fc0c2fe48
Thushan Dananjana <62166556+Thushan98@users.noreply.github.com>
2022-08-07 20:48:19 +0530

    Initiate the branch with required files

---
+++ src/components/anons/hellosection/HelloSection.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import '../../../assets/styles/partials/anons/_hellosection.scss';
+
+function HelloSection() {
+    return(
+        <button></button>
+    );
+}
+export default HelloSection;
---
+++ src/components/anons/hellosection/JoinWithUs.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import '../../../assets/styles/partials/anons/_joinwithbutton.scss';
+
+function JoinWithUs() {
+    return(
+        <button></button>
+    );
+}
+export default JoinWithUs;
