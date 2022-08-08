d5fe11293f8f478e896f049556fd903f58543b65
Thushan Dananjana <62166556+Thushan98@users.noreply.github.com>
2022-08-08 21:10:27 +0530

    bg-image added

--- src/components/anons/hellosection/HelloSection.jsx
+++ src/components/anons/hellosection/HelloSection.jsx
@@ -4,7 +4,7 @@ import '../../../assets/styles/partials/anons/_hellosection.scss';
 
 function HelloSection() {
     return(
-        <Row className="hello-section" lg={12} md={6}>
+        <Row className="hello-section mx-0 mt-4 py-4" lg={12} md={6}>
 
         </Row>
     );
