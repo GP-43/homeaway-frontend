1d6270ff827fbfcd823287f193f2efb4984a4fab
Thushan Dananjana <62166556+Thushan98@users.noreply.github.com>
2022-08-12 20:13:10 +0530

    Array Data added

--- src/components/anons/aboutsection/ClientCard.jsx
+++ src/components/anons/aboutsection/ClientCard.jsx
@@ -7,16 +7,15 @@ function ClientCard() {
   return (
     <Row>
         <Carousel className='client-msg-card bg-dark'>
-            {/*{SliderData.map((slide,id) => (*/}
+            {SliderData.map((slide,id) => (
             <Carousel.Item className="msg-card bg-light mt-5 ms-5 ps-1">
-                <img src="a.png" className="msg-prof-image" />
-                {/* {slide.src} {slide.msg} {slide.name} {slide.location}*/}
-                <p className="text right mt-5 me-1 pt-1">"Your msg hereYour msg hereYour msg hereYour msg hereYour msg hereY
-                    our msg hereYour msg hereYour msg here"</p>
-                <h3 className="mt-2 text right">Name</h3>
-                <h6 className="mt-2 text-muted">Location</h6>
+                <img src={slide.src} className="msg-prof-image" />
+
+                <p className="text right mt-5 me-1 pt-1">"{slide.msg}"</p>
+                <h3 className="mt-2 text right">{slide.name}</h3>
+                <h6 className="mt-2 text-muted">{slide.location}</h6>
             </Carousel.Item>
-            {/*))}*/}
+            ))}
         </Carousel>
     </Row>
   );
