2e9f45b84c1263345b7550a2fa607bc88ca93eff
Thushan Dananjana <62166556+Thushan98@users.noreply.github.com>
2022-08-13 23:46:40 +0530

    css updated.

--- src/components/anons/aboutsection/ClientCard.jsx
+++ src/components/anons/aboutsection/ClientCard.jsx
@@ -4,19 +4,18 @@ import {SliderData} from "./SliderData";
 
 function ClientCard() {
     return (
-    <Row>
-        <Carousel className='client-msg-card bg-transparent'>
-            {SliderData.map((slide,id) => (
-                <Carousel.Item className="msg-card bg-light mt-5 ms-5 ps-1">
-                    <img src={slide.src} className="msg-prof-image" />
-
-                    <p className="text right mt-5 me-1 pt-1">"{slide.msg}"</p>
-                    <h3 className="mt-2 text right">{slide.name}</h3>
-                    <h6 className="mt-2 text-muted">{slide.location}</h6>
-                </Carousel.Item>
-            ))}
-        </Carousel>
-    </Row>
+        <Row>
+            <Carousel className='client-msg-card bg-transparent'>
+                {SliderData.map((slide) => (
+                    <Carousel.Item className="msg-card bg-light mt-5 ms-5 ps-1">
+                        <img src={slide.src} className="msg-prof-image" />
+                        <p className="text right mt-5 me-1 pt-1">"{slide.msg}"</p>
+                        <h3 className="mt-2 text right">{slide.name}</h3>
+                        <h6 className="mt-2 text-muted">{slide.location}</h6>
+                    </Carousel.Item>
+                ))}
+            </Carousel>
+        </Row>
     )
 }
 
