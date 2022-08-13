283503ee06ad41c9b946627e9ca439f6820ac18d
Thushan Dananjana <62166556+Thushan98@users.noreply.github.com>
2022-08-13 23:00:48 +0530

    Update ClientCard.jsx

--- src/components/anons/aboutsection/ClientCard.jsx
+++ src/components/anons/aboutsection/ClientCard.jsx
@@ -3,37 +3,21 @@ import { Row,Carousel } from 'react-bootstrap';
 import {SliderData} from "./SliderData";
 
 function ClientCard() {
-    // let slideIndex = 1;
-    // //showSlides(slideIndex);
-    //
-    // function plusSlides(n) {
-    //     showSlides(slideIndex += n);
-    // };
-    // const showSlides = (n) => {
-    //     let i;
-    //     let slides = document.getElementsByClassName("mySlides");
-    //     if (n > slides.length) {slideIndex = 1}
-    //     if (n < 1) {slideIndex = slides.length}
-    //     for (i = 0; i < slides.length; i++) {
-    //         slides[i].style.display = "none";
-    //     }
-    //     slides[slideIndex-1].style.display = "block";
-    // };
-  return (
+    return (
     <Row>
         <Carousel className='client-msg-card bg-transparent'>
             {SliderData.map((slide,id) => (
-            <Carousel.Item className="msg-card bg-light mt-5 ms-5 ps-1">
-                <img src={slide.src} className="msg-prof-image" />
+                <Carousel.Item className="msg-card bg-light mt-5 ms-5 ps-1">
+                    <img src={slide.src} className="msg-prof-image" />
 
-                <p className="text right mt-5 me-1 pt-1">"{slide.msg}"</p>
-                <h3 className="mt-2 text right">{slide.name}</h3>
-                <h6 className="mt-2 text-muted">{slide.location}</h6>
-            </Carousel.Item>
+                    <p className="text right mt-5 me-1 pt-1">"{slide.msg}"</p>
+                    <h3 className="mt-2 text right">{slide.name}</h3>
+                    <h6 className="mt-2 text-muted">{slide.location}</h6>
+                </Carousel.Item>
             ))}
         </Carousel>
     </Row>
-  );
+    )
 }
 
 export default ClientCard;
