7e64c16818bcdf7eaa827cb075b9e509b9a1e7a6
Tharindu69 <2019cs169@stu.ucsc.cmb.ac.lk>
2022-08-13 23:39:07 +0530

    errors done

--- src/components/admin/occupants/OccupantTable.jsx
+++ src/components/admin/occupants/OccupantTable.jsx
@@ -13,8 +13,9 @@ import p6 from "../../../assets/images/admin/occupant/p6.jpg";
 import p7 from "../../../assets/images/admin/occupant/p7.jpg";
 
 function OccupantTable() {
-
-    const [searchTerm, setSearchTerm] = useState('')
+    const [nameSearchTerm, setNameSearchTerm] = useState('')
+    const [dateSearchTerm, setDateSearchTerm] = useState('')
+    const [rateSearchTerm, setRateSearchTerm] = useState('')
 
     const details_array = [
         {Src : p7, firstName : "saman", lastName : "kumara", email : "saman@gmail.com", joinedDate : '2022-07-15',
@@ -38,13 +39,13 @@ function OccupantTable() {
             <Row className='mx-0 search-part mt-3 pt-3 '>
                 <Col className='px-0 ps-3'>
                     <input className='search-by-name p-2' type="text" placeholder='Search...'
-                           onChange={event => {setSearchTerm(event.target.value)}}
+                           onChange={event => {setNameSearchTerm(event.target.value)}}
                     />
                 </Col>
 
                 <Col className='text-right'>
-                    <select className='search-by-name p-2'  onChange={event => {setSearchTerm(event.target.value)}}> 
-                        <option> Search by ratings ...</option>
+                    <select className='search-by-name p-2'  onChange={event => {setRateSearchTerm(event.target.value)}}> 
+                        <option  hidden value="none"> Search by ratings ...</option>
                         <option>1</option>
                         <option>2</option> 
                         <option>3</option>
@@ -54,7 +55,7 @@ function OccupantTable() {
                 </Col>
                 <Col className='text-center ps-5 ms-5'>
                     <input className='search-by-name p-2' type="date"
-                           onChange={event => {setSearchTerm(event.target.value)}}
+                           onChange={event => {setDateSearchTerm(event.target.value)}}
                     />
                 </Col>
                 <Col className='text-right'></Col>
@@ -78,15 +79,67 @@ function OccupantTable() {
              <Row className='mx-0 px-0 data-part'> 
 
                     {details_array.filter((val) => {
-                    if (searchTerm == ""){
+                    // without searching
+                    if (nameSearchTerm == "" && rateSearchTerm == "" && dateSearchTerm == "")
+                    {
                         return val
-                    } else if (val.firstName.toLowerCase().includes(searchTerm.toLowerCase()) || 
-                        val.lastName.toLowerCase().includes(searchTerm.toLowerCase()) || 
-                        val.joinedDate.includes(searchTerm) ||
-                        val.rate == searchTerm ||
-                        (searchTerm == '4+' && val.rate > 4) ) {
-                            return val 
                     } 
+                    // search by name
+                    else if ((val.firstName.toLowerCase().includes(nameSearchTerm.toLowerCase()) || val.lastName.toLowerCase().includes(nameSearchTerm.toLowerCase()) ) && rateSearchTerm == "" && dateSearchTerm == "")
+                    {
+                        return val
+                    }
+                    // search by rate
+                    else if (nameSearchTerm == "" && dateSearchTerm == "")
+                    {
+                        if(rateSearchTerm == val.rate)
+                        {
+                            return val
+                        }
+                        else if(rateSearchTerm === '4+' && val.rate > 4)
+                        {
+                            return val
+                        }
+                    }
+                    //search by date
+                    else if (nameSearchTerm == "" && rateSearchTerm=="" && dateSearchTerm == val.joinedDate)
+                    {
+                        return val
+                    }
+                    //search by date + rate + name
+                    else if (rateSearchTerm == val.rate && dateSearchTerm == val.joinedDate && (val.firstName.toLowerCase().includes(nameSearchTerm.toLowerCase()) || val.lastName.toLowerCase().includes(nameSearchTerm.toLowerCase()) ) )
+                    {
+                        return val
+                    }
+                    // search by date + name
+                    else if (rateSearchTerm =="" && dateSearchTerm == val.joinedDate && (val.firstName.toLowerCase().includes(nameSearchTerm.toLowerCase()) || val.lastName.toLowerCase().includes(nameSearchTerm.toLowerCase()) ) )
+                    {
+                        return val
+                    }
+                    //search by rate + name
+                    else if (dateSearchTerm == "" && (val.firstName.toLowerCase().includes(nameSearchTerm.toLowerCase()) || val.lastName.toLowerCase().includes(nameSearchTerm.toLowerCase()) )  )
+                    {
+                        if(rateSearchTerm == val.rate)
+                        {
+                            return val
+                        }
+                        else if(rateSearchTerm === '4+' && val.rate > 4)
+                        {
+                            return val
+                        }
+                    }
+                    //search by date + rate
+                    else if(nameSearchTerm == "" && dateSearchTerm == val.joinedDate)
+                    {
+                        if(rateSearchTerm == val.rate)
+                        {
+                            return val
+                        }
+                        else if(rateSearchTerm === '4+' && val.rate > 4)
+                        {
+                            return val
+                        }
+                    }
                 }).map((val, key) => {
                     return (
                         <Row className='data mx-0 px-0 ' key={key}>
