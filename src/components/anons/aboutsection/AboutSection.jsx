f0d18a57f65b605630533b4c5163017f16b45cf6
Thushan Dananjana <62166556+Thushan98@users.noreply.github.com>
2022-08-14 22:56:02 +0530

    412px responsive done

--- src/components/anons/aboutsection/AboutSection.jsx
+++ src/components/anons/aboutsection/AboutSection.jsx
@@ -5,14 +5,14 @@ import { Col, Container, Row } from 'react-bootstrap';
 function AboutSection() {
     return (
         <Container className='pt-1 mt-1 about-section'>
-            <Row>
+
                 <Col xs={4} className="ms-3 me-5 column-about">
                     <Content />
                 </Col>
                 {/*<Col xs={6} className="ms-5">*/}
                 {/*    /!*<SliderData />*!/*/}
                 {/*</Col>*/}
-            </Row>
+            
         </Container>
     )
 }
