cec1163cb3a6b10f2450857fe4ca5e2908e61457
Tharindu69 <2019cs169@stu.ucsc.cmb.ac.lk>
2022-08-18 12:03:37 +0530

    commit kala

--- src/components/user/place_information/PlaceInfoDescription.jsx
+++ src/components/user/place_information/PlaceInfoDescription.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
-import {Row, Col} from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
 
 function PlaceInfoDescription() {
     return (
         <Row>
             <Col xs={12} className='px-0'>
-                 <h3>Description</h3>
+                <h3>Description</h3>
+
+                <Card className='place-description-details'>
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of the card's content.
+                        </Card.Text>
+                        <Card.Link href="#">Card Link</Card.Link>
+                        <Card.Link href="#">Another Link</Card.Link>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of the card's content.
+                        </Card.Text>
+                        <Card.Link href="#">Card Link</Card.Link>
+                        <Card.Link href="#">Another Link</Card.Link>
+                    </Card.Body>
+                </Card>
+
             </Col>
 
         </Row>
+
     );
 }
 
--- src/components/user/place_information/PlaceInfoTime.jsx
+++ src/components/user/place_information/PlaceInfoTime.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import {Row, Col} from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
 
 function PlaceInfoTime() {
     return (
         <Row>
             <Col xs={12} className='px-0'>
                 <h3>Available times</h3>
+
+                <Card className='place-description-details'>
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of the card's content.
+                        </Card.Text>
+                        <Card.Link href="#">Card Link</Card.Link>
+                        <Card.Link href="#">Another Link</Card.Link>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of the card's content.
+                        </Card.Text>
+                        <Card.Link href="#">Card Link</Card.Link>
+                        <Card.Link href="#">Another Link</Card.Link>
+                    </Card.Body>
+                </Card>
             </Col>
 
         </Row>
