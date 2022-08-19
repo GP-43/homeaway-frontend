5c634d6d8a520d0b7d39ef9a6213f990948c9a4a
Thushan Dananjana <62166556+Thushan98@users.noreply.github.com>
2022-08-19 20:31:07 +0530

    updated

--- src/components/user/finding_place/FilterSection.jsx
+++ src/components/user/finding_place/FilterSection.jsx
@@ -5,9 +5,9 @@ function FilterSection() {
     
     return (
         <Row className='mx-0 filter-section bg-transparent'>
-            <Col className='bg-white filter-section-ac pb-2 pt-3 w-100' lg={4} md={6}>
-                <Card className="bg-white filter-card mb-2">
-                    <Card.Body className='py-2 filter-card-body'>
+            <Col className='bg-white filter-section-ac pb-1 pt-3 w-100' lg={4} md={6}>
+                <Card className="bg-white filter-card mb-1">
+                    <Card.Body className='py-1 filter-card-body'>
                         {/*<Card.Title>Card Title</Card.Title>*/}
                         {/*<Card.Subtitle className="mb-0 text-filter">Features</Card.Subtitle>*/}
                         <Card.Text className='radio-button-section w-70'>
@@ -76,57 +76,6 @@ function FilterSection() {
                     </Card.Body>
                 </Card>
             </Col>
-            {/*<Col className='bg-white filter-section-food pb-2 pt-3' lg={4} md={6}>*/}
-            {/*    <Card className="bg-white filter-card mb-2">*/}
-            {/*        <Card.Body className='py-2 filter-card-body'>*/}
-            {/*            /!*<Card.Title>Card Title</Card.Title>*!/*/}
-            {/*            <Card.Subtitle className="mb-0 text-filter">Food and Bevarage</Card.Subtitle>*/}
-            {/*            <Card.Text className='radio-button-section '>*/}
-            {/*                <input */}
-            {/*                    type="radio" */}
-            {/*                    className="filter-radio-button" */}
-            {/*                    id="fdNeed" */}
-            {/*                    name="foodType" */}
-            {/*                    value="fdNeed"*/}
-            {/*                />*/}
-            {/*                <lable className="radio-lable">Need</lable>*/}
-            {/*                <input */}
-            {/*                    type="radio" */}
-            {/*                    className="filter-radio-button" */}
-            {/*                    id="fdNoNeed" */}
-            {/*                    name="foodType" */}
-            {/*                    value="fdNoNeed"*/}
-            {/*                />*/}
-            {/*                <lable className="radio-lable">No Need</lable>*/}
-            {/*            </Card.Text>*/}
-            {/*        </Card.Body>*/}
-            {/*    </Card>*/}
-            {/*</Col>*/}
-            {/*<Col className='bg-white filter-section-internet pb-2 pt-3' lg={4}>*/}
-            {/*    <Card className="bg-white filter-card mb-2">*/}
-            {/*        <Card.Body className='py-2 filter-card-body'>*/}
-            {/*            /!*<Card.Title>Card Title</Card.Title>*!/*/}
-            {/*            <Card.Subtitle className="mb-0 text-filter">Internet Access</Card.Subtitle>*/}
-            {/*            <Card.Text className='radio-button-section'>*/}
-            {/*                <input */}
-            {/*                    type="radio" */}
-            {/*                    className="filter-radio-button" */}
-            {/*                    id="iNeed" name="internetType" */}
-            {/*                    value="iNeed"*/}
-            {/*                />*/}
-            {/*                <lable className="radio-lable">Need</lable>*/}
-            {/*                <input */}
-            {/*                    type="radio" */}
-            {/*                    className="filter-radio-button" */}
-            {/*                    id="iNoNeed" */}
-            {/*                    name="internetType" */}
-            {/*                    value="iNoNeed"*/}
-            {/*                />*/}
-            {/*                <lable className="radio-lable">No Need</lable>*/}
-            {/*            </Card.Text>*/}
-            {/*        </Card.Body>*/}
-            {/*    </Card>*/}
-            {/*</Col>*/}
         </Row>
     );
 }
