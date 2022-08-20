5a5650792364e37df4f628c75c7f9a5a23a49de5
Thushan Dananjana <62166556+Thushan98@users.noreply.github.com>
2022-08-20 09:23:38 +0530

    update about section

--- src/components/anons/aboutsection/AboutSection.jsx
+++ src/components/anons/aboutsection/AboutSection.jsx
@@ -6,9 +6,12 @@ function AboutSection() {
     return (
         <Container className='pt-1 mt-1 about-section'>
                 <Row className='about-section-title text-center'><div>About{' '}<span>us.</span></div></Row>
-                <Row className='about-section-description text-center'>Of course, you have a homepage and dedicated pages for your
-                    products, but summarizing your offerings on the About Us page is crucial to tie them in with brand
-                    values in your messaging</Row>
+                <Row className='about-section-description text-center'>Established in 2022, HomeAway is currently Sri
+                    Lanka's most visited property and apartment renting website with over 10000 active ads.
+                    HomeAway is a one stop e-platform for selective lifestyles needs – Help to find a location to meet
+                    and work with your favourite collegues or business partners or team. We are expanding and growing
+                    to help you meet your lifestyle needs.
+                </Row>
                 <Row xs={4} className="ms-3 me-5 column-about">
                     <Content />
                 </Row>
