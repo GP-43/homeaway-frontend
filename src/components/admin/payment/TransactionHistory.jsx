15a37efe52c5388985548a27ba8e93114945d309
Tharaki-99 <dilshanitharaki@gmail.com>
2022-08-21 13:50:01 +0530

    done

--- src/components/admin/payment/TransactionHistory.jsx
+++ src/components/admin/payment/TransactionHistory.jsx
@@ -3,34 +3,25 @@ import { Row, Col } from 'react-bootstrap';
 import TransactionRow from "./TransactionRow";
 import { useState } from 'react';
 
-//images
-import p1 from "../../../assets/images/admin/occupant/p1.jpg";
-import p2 from "../../../assets/images/admin/occupant/p2.jpg";
-import p3 from "../../../assets/images/admin/occupant/p3.jpg";
-import p4 from "../../../assets/images/admin/occupant/p4.jpg";
-import p5 from "../../../assets/images/admin/occupant/p5.jpg";
-import p6 from "../../../assets/images/admin/occupant/p6.jpg";
-import p7 from "../../../assets/images/admin/occupant/p7.jpg";
-
 function TransactionHistory() {
     const [nameSearchTerm, setNameSearchTerm] = useState('')
     const [dateSearchTerm, setDateSearchTerm] = useState('')
 
     const Transaction_array = [
-        {Src : p7, firstName : "saman", lastName : "kumara", email : "saman@gmail.com", joinedDate : '2022-07-15',
-            rate : '2'},
-        {Src : p1, firstName : "namal", lastName : "perera", email : "saman@gmail.com", joinedDate : '2022-08-15',
-            rate : '4'},
-        {Src : p2, firstName : "amal", lastName : "saman", email : "saman@gmail.com", joinedDate : '2022-08-16',
-            rate : '4'},
-        {Src : p3, firstName : "ruwan", lastName : "pushpa", email : "saman@gmail.com", joinedDate : '2022-08-17',
-            rate : '3'},
-        {Src : p4, firstName : "sandum", lastName : "nimal", email : "saman@gmail.com", joinedDate : '2022-08-18',
-            rate : '1'},
-        {Src : p5, firstName : "pawan", lastName : "pahan", email : "saman@gmail.com", joinedDate : '2022-08-19',
-            rate : '4'},
-        {Src : p6, firstName : "nilantha", lastName : "dasun", email : "saman@gmail.com", joinedDate : '2022-08-18',
-            rate : '6'}
+        {RenterName : "Saman Kumara", RenterEmail : "saman@gmail.com", RentDate : '2022-07-15',
+            Amount : '3500', OccupantName : "Sandum Nimal" , OccupantEmail : "saman@gmail.com", Status : "Success"},
+        {RenterName : "Namal Perera", RenterEmail : "saman@gmail.com", RentDate : '2022-08-15',
+            Amount : '8500', OccupantName : "Sandum Nimal" , OccupantEmail : "saman@gmail.com", Status : "Success"},
+        {RenterName : "Amal Saman", RenterEmail : "saman@gmail.com", RentDate : '2022-08-16',
+            Amount : '10000', OccupantName : "Sandum Nimal" , OccupantEmail : "saman@gmail.com", Status : "Success"},
+        {RenterName : "Ruwan Pushpa", RenterEmail : "saman@gmail.com", RentDate : '2022-08-17',
+            Amount : '6800', OccupantName : "Sandum Nimal" , OccupantEmail : "saman@gmail.com", Status : "Success"},
+        {RenterName : "Sandum Nimal", RenterEmail : "saman@gmail.com", RentDate : '2022-08-18',
+            Amount : '3900', OccupantName : "Sandum Nimal" , OccupantEmail : "saman@gmail.com", Status : "Success"},
+        {RenterName : "Pawan Pahan", RenterEmail : "saman@gmail.com", RentDate : '2022-08-19',
+            Amount : '1400', OccupantName : "Sandum Nimal" , OccupantEmail : "saman@gmail.com", Status : "Success"},
+        {RenterName : "Nilantha Dasun", RenterEmail : "saman@gmail.com", RentDate : '2022-08-18',
+            Amount : '6700', OccupantName : "Sandum Nimal" , OccupantEmail : "saman@gmail.com", Status : "Unsuccess"}
     ]
 
     return (
@@ -62,17 +53,17 @@ function TransactionHistory() {
                         return val
                     } 
                     // search by name
-                    else if ((val.firstName.toLowerCase().includes(nameSearchTerm.toLowerCase()) || val.lastName.toLowerCase().includes(nameSearchTerm.toLowerCase()) ) && dateSearchTerm == "")
+                    else if ((val.RenterName.toLowerCase().includes(nameSearchTerm.toLowerCase()) || val.OccupantName.toLowerCase().includes(nameSearchTerm.toLowerCase()) ) && dateSearchTerm == "")
                     {
                         return val
                     }                    
                     //search by date
-                    else if (nameSearchTerm == "" && dateSearchTerm == val.joinedDate)
+                    else if (nameSearchTerm == "" && dateSearchTerm == val.RentDate)
                     {
                         return val
                     }
                     // search by date + name
-                    else if (dateSearchTerm == val.joinedDate && (val.firstName.toLowerCase().includes(nameSearchTerm.toLowerCase()) || val.lastName.toLowerCase().includes(nameSearchTerm.toLowerCase()) ) )
+                    else if (dateSearchTerm == val.RentDate && (val.RenterName.toLowerCase().includes(nameSearchTerm.toLowerCase()) || val.OccupantName.toLowerCase().includes(nameSearchTerm.toLowerCase()) ) )
                     {
                         return val
                     }
@@ -80,14 +71,15 @@ function TransactionHistory() {
                     
                 }).map((val, key) => {
                     return (
-                        <Row className='data mx-0 px-0 ' key={key}>
+                        <Row className='data my-4' key={key}>
                             <TransactionRow
-                                Src = {val.Src}
-                                firstName = {val.firstName}
-                                lastName = {val.lastName}
-                                email = {val.email}
-                                joinedDate = {val.joinedDate}
-                                rate = {val.rate}
+                                RenterName = {val.RenterName}
+                                RenterEmail= {val.RenterEmail}
+                                RentDate= {val.RentDate}
+                                Amount= {val.Amount}
+                                OccupantName= {val.OccupantName}
+                                OccupantEmail= {val.OccupantEmail}
+                                Status= {val.Status}
                             />
                         </Row>
                     )
--- src/components/admin/payment/TransactionRow.jsx
+++ src/components/admin/payment/TransactionRow.jsx
@@ -1,69 +1,49 @@
 import React from 'react';
 import { Row, Col } from 'react-bootstrap';
-// for popup
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
 
-//popup function
-function DeletePopup(props) {
+function TransactionRow(props) {
     return (
-      <Modal {...props} size="lg" aria-labelledby="contained-modal-title-vcenter" centered >
-        <Modal.Header closeButton >
-          <Modal.Title id="contained-modal-title-vcenter">
-              Confirm Deletion
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body >
-          <p>Do you want delete? This action cannot be undone.</p>
-        </Modal.Body>
-        <Modal.Footer >
-            <Button className='confirm-delete btn-danger'>Delete</Button>
-          <Button onClick={props.onHide}>Cancel</Button>
-        </Modal.Footer>
-      </Modal>
-    );
-  }
+        <Col className='ms-4 ps-4 py-3 singlerow'>
+            <Row className='rentername pb-2'>
+                {props.RenterName}
+            </Row>
+            <Row className='bottomrow'>
+                <Col className='mx-0' xs={4}>
+                    <Row className=''>
+                        <Col xs= {5} className='top-data'>Rent Date:</Col>
+                        <Col xs= {6} className='test-data'>{props.RentDate}</Col>
+                    </Row>
+                    <Row >
+                        <Col xs= {5} className='top-data'>Email:</Col>
+                        <Col xs= {6} className='test-data'>{props.RenterEmail}</Col>                        
+                    </Row>
+                    <Row>
+                        <Col xs= {5} className='top-data'>Amount:</Col>
+                        <Col xs= {6} className='test-data'>{props.Amount}</Col>                        
+                    </Row>
+                </Col>
+                <Col xs={4} className='ms-4 p-0'>
+                    <Row>
+                        <Col xs= {6} className='test-data-top top-data'>Renter Name:</Col>
+                        <Col xs= {6} className='test-data'>{props.OccupantName}</Col>
+                        
+                    </Row>
+                    <Row>
+                        <Col xs= {6} className='test-data-top top-data'>Email:</Col>
+                        <Col xs= {6} className='test-data'>{props.OccupantEmail}</Col> 
+                    </Row>
+                </Col>
+                <Col xs={3} className=''>
+                    <Col xs= {5} className='test-data-top top-data'>Status:</Col>
+                    <Col xs= {5} className='test-data'>{props.Status}</Col> 
+                </Col>
+
+            </Row>
+        </Col>
 
 
-//data row
 
-function OccupantRow(props) {
-
-    const [modalShow, setModalShow] = React.useState(false);
-
-    return (
-        
-        <Row className='row-top-products mx-0 px-0 mb-2 mt-3'>
-            <Col className='px-0 ps-3' xs = {1}>
-                <img src={props.Src} className = "row-top-products-img" />
-            </Col>
-
-           <Col className='ps-2 pe-0 name' xs = {2}>
-                <Row>
-                    {props.firstName} {props.lastName}
-                </Row>
-                <Row className='ps-2 rate'>
-                    {props.rate}
-                </Row>                
-            </Col>
-
-            <Col className='px-0' xs = {3}>
-                {props.email}
-            </Col>
-
-            <Col className='px-0' xs = {4}>
-                {props.joinedDate}
-            </Col>
-
-            <Col className='px-0 ' xs = {2}>
-                <button className='delete-btn px-3 pb-0' variant="primary" onClick={() => setModalShow(true)}>
-                    <p>DELETE</p>
-                </button>
-                
-                <DeletePopup show={modalShow} onHide={() => setModalShow(false)} />
-            </Col>
-        </Row>
     )
 }
 
-export default OccupantRow
+export default TransactionRow
