50d1f7bed6e391a48a16f287844e8ef85e2351a5
Tharindu69 <2019cs169@stu.ucsc.cmb.ac.lk>
2022-08-22 21:46:34 +0530

    buttons work well

--- src/views/pages/user/AddNewPlace.jsx
+++ src/views/pages/user/AddNewPlace.jsx
@@ -7,52 +7,71 @@ import ProgressBar from "../../../components/user/add_new_place/ProgressBar";
 import AddNewRent from "../../../components/user/add_new_rening/AddNewRent";
 
 function AddNewPlace() {
-    const [addNewPlaceFormFirst, setaddNewPlaceFormFirst] = useState(false);
-    const firstClick = () => {
-        setaddNewPlaceFormFirst(true);
+    const [addNewPlaceFormFirstNext, setaddNewPlaceFormFirstNext] = useState(false);
+    const firstNextClick = () => {
+        setaddNewPlaceFormFirstNext(!addNewPlaceFormFirstNext);
     }
 
-    const [addNewPlaceFormSecond, setaddNewPlaceFormSecond] = useState(false);
-    const secondClick = () => {
-        setaddNewPlaceFormSecond(true);
+    const [addNewPlaceFormSecondNext, setaddNewPlaceFormSecondNext] = useState(false);
+    const secondNextClick = () => {
+        setaddNewPlaceFormSecondNext(!addNewPlaceFormSecondNext);
     }
 
-    const [addNewPlaceFormThird, setaddNewPlaceFormThird] = useState(false);
-    const thirdClick = () => {
-        setaddNewPlaceFormThird(true);
+    const [addNewPlaceFormThirdNext, setaddNewPlaceFormThirdNext] = useState(false);
+    const thirdNextClick = () => {
+        setaddNewPlaceFormThirdNext(!addNewPlaceFormThirdNext);
     }
 
-    const [isNextClicked, setIsNextClicked] = useState(false);
+
+    const [addNewPlaceFormFirstBack, setaddNewPlaceFormFirstBack] = useState(false);
+    const firstBackClick = () => {
+        setaddNewPlaceFormFirstBack(!addNewPlaceFormFirstBack);
+        setaddNewPlaceFormFirstNext(!addNewPlaceFormFirstNext);
+
+    }
+
+
+    const [addNewPlaceFormSecondBack, setaddNewPlaceFormSecondBack] = useState(false);
+    const secondBackClick = () => {
+        setaddNewPlaceFormSecondBack(!addNewPlaceFormSecondBack);
+        setaddNewPlaceFormSecondNext(!addNewPlaceFormSecondNext);
+    }
 
     return (
         <Container className="add-new-rent-container bg-white">
 
-            <ProgressBar First={addNewPlaceFormFirst} Second={addNewPlaceFormSecond} Third={addNewPlaceFormThird} />
+            <ProgressBar FirstNext={addNewPlaceFormFirstNext}
+                SecondNext={addNewPlaceFormSecondNext}
+                ThirdNext={addNewPlaceFormThirdNext}
+                FirstBack={addNewPlaceFormFirstBack}
+                SecondBack={addNewPlaceFormSecondBack}
+            />
 
-            <AddNewRent />
-
-            <Row className="d-block">
+            <Row className= {addNewPlaceFormFirstNext && "d-none"} >
+                <AddNewRent />
+            </Row>
+            <Row className={addNewPlaceFormSecondNext && "d-none"}>
                 <AddNewRentFeatureSelection />
             </Row>
-            <Row className="mb-2 add-new-rent-cancel-btn-box">
+            <Row className={ addNewPlaceFormFirstNext ? "d-none" : "mb-2 add-new-rent-cancel-btn-box"}>
                 <Col >
-                    <button className="pt-2 pb-2 add-new-rent-next-btn" onClick={firstClick} >NEXT</button>
+                    <button className="pt-2 pb-2 add-new-rent-next-btn" onClick={firstNextClick} >NEXT</button>
                 </Col>
             </Row>
-            <Row className="mb-2 add-new-rent-cancel-btn-box">
+            <Row className={ (addNewPlaceFormFirstNext && !addNewPlaceFormThirdNext && !addNewPlaceFormSecondNext) ? "mb-2 add-new-rent-cancel-btn-box" : "d-none"}>
                 <Col >
-                    <button className="pt-2 pb-2 add-new-rent-cancel-btn">BACK</button>
+                    <button className="pt-2 pb-2 add-new-rent-cancel-btn" onClick={firstBackClick} >BACK</button>
                 </Col>
                 <Col >
-                    <button className="pt-2 pb-2 add-new-rent-next-btn" onClick={secondClick}>NEXT</button>
+                    <button className="pt-2 pb-2 add-new-rent-next-btn" onClick={secondNextClick}>NEXT</button>
                 </Col>
             </Row>
-            <Row className="mb-2 add-new-rent-cancel-btn-box">
+            <Row className={ (addNewPlaceFormFirstNext && addNewPlaceFormSecondNext) ? "mb-2 add-new-rent-cancel-btn-box" : "d-none" }>
                 <Col >
-                    <button className="pt-2 pb-2 add-new-rent-cancel-btn">BACK</button>
+                    <button className="pt-2 pb-2 add-new-rent-cancel-btn" onClick={secondBackClick} >BACK</button>
                 </Col>
                 <Col >
-                    <button type="submit" className="pt-2 pb-2 add-new-rent-next-btn" onClick={thirdClick}>SUBMIT</button>
+                    <button type="submit" className="pt-2 pb-2 add-new-rent-next-btn" onClick={thirdNextClick}>SUBMIT</button>
                 </Col>
             </Row>
         </Container>
