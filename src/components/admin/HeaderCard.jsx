0eef7b331605a2aa30dbfa7a97e5c21dcec09cf3
Tharaki-99 <dilshanitharaki@gmail.com>
2022-08-24 21:34:52 +0530

    done

--- src/components/admin/HeaderCard.jsx
+++ src/components/admin/HeaderCard.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import {Card, Col, Container, Row} from "react-bootstrap";
-import {FaHome} from "react-icons/fa";
-import {FaUserCircle} from "react-icons/fa";
+import { Card, Col, Container, Row } from "react-bootstrap";
+import { FaHome } from "react-icons/fa";
+import { FaUserCircle } from "react-icons/fa";
 import Button from "react-bootstrap/Button";
-import {FiLogOut} from "react-icons/fi";
+import { FiLogOut } from "react-icons/fi";
 
 function HeaderCard() {
     const handleOnLogoClick = () => {
@@ -14,20 +14,20 @@ function HeaderCard() {
         <Card className="Header-card">
             <Col className="d-flex flex-row  Header-card-div">
                 <a href="" className="Header-card-div-text ms-2 mt-2">
-                    <FaHome className="header-card-home-icon " size="lg"/>
+                    <FaHome className="header-card-home-icon " size="lg" />
                 </a>
-                <h7 className="Header-card-div-text ">/</h7>
+                <h6 className="Header-card-div-text ">/</h6>
                 <a href="" className="Header-card-div-text ">
-                    <h7 className="Header-card-div-text ms-0 mt-0">Dashboard</h7>
+                    <h6 className="Header-card-div-text ms-0 mt-0">Dashboard</h6>
                 </a>
             </Col>
             <Row>
                 <Col xs={10} className="Header-card-div">
                     <h4 className="Header-card-div-text ">Dashboard</h4>
                 </Col>
-                <Col xs={1} className="user-icon-box"><FiLogOut className="user-icon"/></Col>
+                <Col xs={1} className="user-icon-box"><FiLogOut className="user-icon" /></Col>
                 <Col xs={1} className="admin-name"><Button className='bg-transparent border-0 py-0'
-                                                           onClick={handleOnLogoClick}>Logout</Button></Col>
+                    onClick={handleOnLogoClick}>Logout</Button></Col>
             </Row>
         </Card>
     );
