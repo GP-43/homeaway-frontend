dbae46f1bb04f19b32bdac892e159870d493c976
Tharindu69 <2019cs169@stu.ucsc.cmb.ac.lk>
2022-09-06 17:07:58 +0530

    started

--- src/components/admin/complaints/AdminComplaintsTable.jsx
+++ src/components/admin/complaints/AdminComplaintsTable.jsx
@@ -1,42 +1,94 @@
-import React from 'react';
-import { Col, Row, Container } from 'react-bootstrap';
+import React, { useState, useEffect } from "react";
+import axois from "axios";
+import { Col, Row, Container } from "react-bootstrap";
 import AdminComplaintsTableRow from "./AdminComplaintsTableRow";
 
-
-
-
 function AdminComplaintsTable() {
-    const complaints_data = [{ caseNo: 1001, subject: "This is a complaint about the owner of the xxxxxxx. ", senderId: 245, senderName: "Kamal Nishantha", senderEmail: "kamalnishantha@gmail.com", date: "22-06-2022", time: "10:30" },
-    { caseNo: 1001, subject: "This is a complaint about the owner of the xxxxxxx.", senderId: 127, senderName: "Ranil Wikrama", senderEmail: "ranilwikrama@gmail.com", date: "02-06-2022", time: "01:45" }]
-    return (
-        <Container className='admin-complaint-table-container p-2'>
-            <Row className='ms-1'>
-                <Row className='admin-complaints-table-title'>
-                    <Col xs={1} className="px-0">
-                        <p className='admin-complain-table-row-title'>Case No</p>
-                    </Col>
-                    <Col xs={2} className="px-0">
-                        <p className='admin-complain-table-row-title'>Subject</p>
-                    </Col>
-                    <Col xs={1} className="px-0">
-                        <p className='admin-complain-table-row-title'>Sender ID</p></Col>
-                    <Col xs={2} className="px-0">
-                        <p className='admin-complain-table-row-title'>Sender Name</p></Col>
-                    <Col xs={3} className="px-0">
-                        <p className='admin-complain-table-row-title'>Sender Email</p></Col>
-                    <Col xs={2} className="px-0">
-                        <p className='admin-complain-table-row-title'>Time & Date</p></Col>
-                    <Col xs={1} className="px-0">
+  const [details, setDetails] = useState({});
 
-                    </Col>
-                </Row>
+  useEffect((event) => {
+    axois
+      .get("http://localhost:4000/admin/select/complaints")
+      .then((data) => {
+        const details = data.data;
+        setDetails({ ...details });
+        //setoccupantData(false)
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-                <Row className='admin-complaint-table-data'>
-                    {complaints_data && complaints_data.map(i => <AdminComplaintsTableRow caseNo={i.caseNo} subject={i.subject} senderId={i.senderId} senderName={i.senderName} senderEmail={i.senderEmail} date={i.date} time={i.time} />)}
-                </Row>
+  const complaints_data = [
+    {
+      caseNo: 1001,
+      subject: "This is a complaint about the owner of the xxxxxxx. ",
+      senderId: 245,
+      senderName: "Kamal Nishantha",
+      senderEmail: "kamalnishantha@gmail.com",
+      date: "22-06-2022",
+      time: "10:30",
+    },
+    {
+      caseNo: 1001,
+      subject: "This is a complaint about the owner of the xxxxxxx.",
+      senderId: 127,
+      senderName: "Ranil Wikrama",
+      senderEmail: "ranilwikrama@gmail.com",
+      date: "02-06-2022",
+      time: "01:45",
+    },
+  ];
+  return (
+    <Container className="admin-complaint-table-container p-2">
+      <Row className="ms-1">
+        <Row className="admin-complaints-table-title">
+          <Col xs={1} className="px-0">
+            <p className="admin-complain-table-row-title">Case No</p>
+          </Col>
+          <Col xs={2} className="px-0">
+            <p className="admin-complain-table-row-title">Subject</p>
+          </Col>
+          <Col xs={1} className="px-0">
+            <p className="admin-complain-table-row-title">Sender ID</p>
+          </Col>
+          <Col xs={2} className="px-0">
+            <p className="admin-complain-table-row-title">Sender Name</p>
+          </Col>
+          <Col xs={3} className="px-0">
+            <p className="admin-complain-table-row-title">Sender Email</p>
+          </Col>
+          <Col xs={2} className="px-0">
+            <p className="admin-complain-table-row-title">Time & Date</p>
+          </Col>
+          <Col xs={1} className="px-0"></Col>
+        </Row>
 
-            </Row>
-        </Container>
-    );
+        <Row className="admin-complaint-table-data">
+          {Object.keys(details).map((key, index) => (
+            <AdminComplaintsTableRow
+              caseNo={details[index].id}
+              complainerId={details[index].compliner_id}
+              complaineeId={details[index].complainee_id}
+              subject={details[index].subject}
+              // Src={i.Src}
+            />
+          ))}
+          {/* {complaints_data &&
+            complaints_data.map((i) => (
+              <AdminComplaintsTableRow
+                caseNo={i.caseNo}
+                subject={i.subject}
+                senderId={i.senderId}
+                senderName={i.senderName}
+                senderEmail={i.senderEmail}
+                date={i.date}
+                time={i.time}
+              />
+            ))} */}
+        </Row>
+      </Row>
+    </Container>
+  );
 }
-export default AdminComplaintsTable
+export default AdminComplaintsTable;
--- src/components/admin/complaints/AdminComplaintsTableRow.jsx
+++ src/components/admin/complaints/AdminComplaintsTableRow.jsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axois from "axios";
 import { Col, Row, Container } from 'react-bootstrap';
 // for popup
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+
+const deleteComplain = () =>{
+    
+    var Id = localStorage.getItem("ComplainID");
+    localStorage.removeItem("ComplainID");
+    console.log(Id);
+    
+    axois
+      .get("http://localhost:4000/admin/delete/complaint/"+Id)
+      .then(() => {
+        console.log("Work");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+}
 
 //popup function
 function DeletePopup(props) {
@@ -17,7 +35,7 @@ function DeletePopup(props) {
           <p>Do you want delete? This action cannot be undone.</p>
         </Modal.Body>
         <Modal.Footer >
-            <Button className='confirm-delete btn-danger'>Delete</Button>
+            <Button className='confirm-delete btn-danger' onClick={deleteComplain}>Reject</Button>
           <Button onClick={props.onHide}>Cancel</Button>
         </Modal.Footer>
       </Modal>
@@ -26,6 +44,8 @@ function DeletePopup(props) {
 
 
 function AdminComplaintsTableRow(props) {
+
+    const Id = props.caseNo;
 
     const [modalShow, setModalShow] = React.useState(false);
 
@@ -38,16 +58,26 @@ function AdminComplaintsTableRow(props) {
                 <p className='admin-complain-table-row-text'>{props.subject}</p>
             </Col>
             <Col xs={1} className="px-0">
-                <p className='admin-complain-table-row-text'>{props.senderId}</p></Col>
+                <p className='admin-complain-table-row-text'>{props.complainerId}</p></Col>
             <Col xs={2} className="px-0">
-                <p className='admin-complain-table-row-text'>{props.senderName}</p></Col>
-            <Col xs={3} className="px-0">
+                <p className='admin-complain-table-row-text'>{props.complaineeId}</p></Col>
+            {/* <Col xs={3} className="px-0">
                 <p className='admin-complain-table-row-text'>{props.senderEmail}</p></Col>
             <Col xs={2} className="px-0">
-                <p className='admin-complain-table-row-text'>{props.date} <br /> {props.time}</p></Col>
+                <p className='admin-complain-table-row-text'>{props.date} <br /> {props.time}</p></Col> */}
             <Col xs={1} className="px-0">
-                <button className='admin-complaint-delete-btn px-3 pb-0' variant="primary" onClick={() => setModalShow(true)}>
-                    <p>DELETE</p>
+                <button className='admin-complaint-accept-btn px-3 pb-0' variant="primary">
+                    <p>ACCEPT</p>
+                </button>
+                
+            </Col>
+            <Col xs={1} className="px-0">
+                <button className='admin-complaint-delete-btn px-3 pb-0' variant="primary" onClick={() =>
+                {
+                    localStorage.setItem("ComplainID", Id);
+                    setModalShow(true)
+                }}>
+                    <p>REJECT</p>
                 </button>
                 
                 <DeletePopup show={modalShow} onHide={() => setModalShow(false)} />
