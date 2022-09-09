5be355175577075355dc6c94397338c0b2ecc6ba
Tharaki-99 <dilshanitharaki@gmail.com>
2022-09-09 09:50:43 +0530

    done

--- src/components/admin/payment/TransactionHistory.jsx
+++ src/components/admin/payment/TransactionHistory.jsx
@@ -1,28 +1,45 @@
 import React from 'react';
 import { Row, Col } from 'react-bootstrap';
 import TransactionRow from "./TransactionRow";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axois from "axios";
 
 function TransactionHistory() {
     const [nameSearchTerm, setNameSearchTerm] = useState('')
     const [dateSearchTerm, setDateSearchTerm] = useState('')
 
-    const Transaction_array = [
-        {RenterName : "Saman Kumara", RenterEmail : "saman@gmail.com", RentDate : '2022-07-15',
-            Amount : '3500', OccupantName : "Sandum Nimal" , OccupantEmail : "saman@gmail.com", Status : "Success"},
-        {RenterName : "Namal Perera", RenterEmail : "saman@gmail.com", RentDate : '2022-08-15',
-            Amount : '8500', OccupantName : "Sandum Nimal" , OccupantEmail : "saman@gmail.com", Status : "Success"},
-        {RenterName : "Amal Saman", RenterEmail : "saman@gmail.com", RentDate : '2022-08-16',
-            Amount : '10000', OccupantName : "Sandum Nimal" , OccupantEmail : "saman@gmail.com", Status : "Success"},
-        {RenterName : "Ruwan Pushpa", RenterEmail : "saman@gmail.com", RentDate : '2022-08-17',
-            Amount : '6800', OccupantName : "Sandum Nimal" , OccupantEmail : "saman@gmail.com", Status : "Success"},
-        {RenterName : "Sandum Nimal", RenterEmail : "saman@gmail.com", RentDate : '2022-08-18',
-            Amount : '3900', OccupantName : "Sandum Nimal" , OccupantEmail : "saman@gmail.com", Status : "Success"},
-        {RenterName : "Pawan Pahan", RenterEmail : "saman@gmail.com", RentDate : '2022-08-19',
-            Amount : '1400', OccupantName : "Sandum Nimal" , OccupantEmail : "saman@gmail.com", Status : "Success"},
-        {RenterName : "Nilantha Dasun", RenterEmail : "saman@gmail.com", RentDate : '2022-08-18',
-            Amount : '6700', OccupantName : "Sandum Nimal" , OccupantEmail : "saman@gmail.com", Status : "Unsuccess"}
-    ]
+    // const Transaction_array = [
+    //     {RenterName : "Saman Kumara", RenterEmail : "saman@gmail.com", RentDate : '2022-07-15',
+    //         Amount : '3500', OccupantName : "Sandum Nimal" , OccupantEmail : "saman@gmail.com", Status : "Success"},
+    //     {RenterName : "Namal Perera", RenterEmail : "saman@gmail.com", RentDate : '2022-08-15',
+    //         Amount : '8500', OccupantName : "Sandum Nimal" , OccupantEmail : "saman@gmail.com", Status : "Success"},
+    //     {RenterName : "Amal Saman", RenterEmail : "saman@gmail.com", RentDate : '2022-08-16',
+    //         Amount : '10000', OccupantName : "Sandum Nimal" , OccupantEmail : "saman@gmail.com", Status : "Success"},
+    //     {RenterName : "Ruwan Pushpa", RenterEmail : "saman@gmail.com", RentDate : '2022-08-17',
+    //         Amount : '6800', OccupantName : "Sandum Nimal" , OccupantEmail : "saman@gmail.com", Status : "Success"},
+    //     {RenterName : "Sandum Nimal", RenterEmail : "saman@gmail.com", RentDate : '2022-08-18',
+    //         Amount : '3900', OccupantName : "Sandum Nimal" , OccupantEmail : "saman@gmail.com", Status : "Success"},
+    //     {RenterName : "Pawan Pahan", RenterEmail : "saman@gmail.com", RentDate : '2022-08-19',
+    //         Amount : '1400', OccupantName : "Sandum Nimal" , OccupantEmail : "saman@gmail.com", Status : "Success"},
+    //     {RenterName : "Nilantha Dasun", RenterEmail : "saman@gmail.com", RentDate : '2022-08-18',
+    //         Amount : '6700', OccupantName : "Sandum Nimal" , OccupantEmail : "saman@gmail.com", Status : "Unsuccess"}
+    // ]
+
+    const [details, setDetails] = useState({});
+
+    useEffect((event) => {
+        axois
+            .get("http://localhost:4000/admin/view/payment")
+            .then((data) => {
+                const details = data.data;
+                setDetails({ ...details });
+                //setoccupantData(false)
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
+
 
     return (
         <Col className='transaction'>
@@ -32,21 +49,38 @@ function TransactionHistory() {
                 </Col>
                 <Col xs={3} className='px-0 ps-3'>
                     <input className='search-by-name p-2' type="text" placeholder='Search...'
-                           onChange={event => {setNameSearchTerm(event.target.value)}}
+                        onChange={event => { setNameSearchTerm(event.target.value) }}
                     />
                 </Col>
                 <Col xs={3} className='text-center ps-5 ms-5'>
                     <input className='search-by-name p-2' type="date"
-                           onChange={event => {setDateSearchTerm(event.target.value)}}
+                        onChange={event => { setDateSearchTerm(event.target.value) }}
                     />
                 </Col>
                 <Col className='text-right'></Col>
                 <Col className='text-right'></Col>
             </Row>
 
-            <Row className='mx-0 px-0 data-part'> 
+            <Row className='mx-0 px-0 data-part'>
 
-                    {Transaction_array.filter((val) => {
+                <Row className='data my-4'>
+
+                    {/* {Object.keys(details).map((key, index) => (
+                        <TransactionRow
+                            RenterName={details[index].name}
+                            RenterEmail={details[index].email}
+                            RentDate={details[index].rent_date}
+                            Amount={details[index].amount}
+                            OccupantName={details[index].renter_name}
+                            OccupantEmail={details[index].renter_email}
+                            Status={details[index].status}
+
+                        />
+                    ))} */}
+                </Row>
+
+
+                {/*{Transaction_array.filter((val) => {
                     // without searching
                     if (nameSearchTerm == "" && dateSearchTerm == "")
                     {
@@ -84,7 +118,44 @@ function TransactionHistory() {
                         </Row>
                     )
                 })}
-             </Row> 
+             </Row>  */}
+
+
+                {Object.keys(details).filter((index) => {
+                    // without searching
+                    if (nameSearchTerm == "" && dateSearchTerm == "") {
+                        return index
+                    }
+                    // search by name
+                    else if ((details[index].name.toLowerCase().includes(nameSearchTerm.toLowerCase()) || details[index].name.toLowerCase().includes(nameSearchTerm.toLowerCase())) && dateSearchTerm == "") {
+                        return index
+                    }
+                    //search by date
+                    else if (nameSearchTerm == "" && dateSearchTerm == details[index].rent_date) {
+                        return index
+                    }
+                    // search by date + name
+                    else if (dateSearchTerm == details[index].rent_date && (details[index].name.toLowerCase().includes(nameSearchTerm.toLowerCase()) || details[index].name.toLowerCase().includes(nameSearchTerm.toLowerCase()))) {
+                        return index
+                    }
+
+
+                }).map((key, index) => {
+                    return (
+                        <Row className='data my-2' index={index}>
+                            <TransactionRow
+                                RenterName={details[index].name}
+                                RenterEmail={details[index].email}
+                                RentDate={details[index].rent_date}
+                                Amount={details[index].amount}
+                                OccupantName={details[index].renter_name}
+                                OccupantEmail={details[index].renter_email}
+                                Status={details[index].status}
+                            />
+                        </Row>
+                    )
+                })}
+            </Row>
         </Col>
     )
 }
--- src/components/admin/payment/TransactionRow.jsx
+++ src/components/admin/payment/TransactionRow.jsx
@@ -3,39 +3,39 @@ import { Row, Col } from 'react-bootstrap';
 
 function TransactionRow(props) {
     return (
-        <Col className='ms-4 ps-4 py-3 singlerow'>
+        <Col xs={11} className='ms-5 ps-4 py-3 px-1 my-1 singlerow'>
             <Row className='rentername pb-2'>
                 {props.RenterName}
             </Row>
             <Row className='bottomrow'>
-                <Col className='mx-0' xs={4}>
+                <Col className='mx-0' xs={5}>
                     <Row className=''>
-                        <Col xs= {5} className='top-data'>Rent Date:</Col>
-                        <Col xs= {6} className='test-data'>{props.RentDate}</Col>
+                        <Col xs={4} className='top-data'>Rent Date:</Col>
+                        <Col xs={8} className='ps-1 test-data'>{props.RentDate}</Col>
                     </Row>
                     <Row >
-                        <Col xs= {5} className='top-data'>Email:</Col>
-                        <Col xs= {6} className='test-data'>{props.RenterEmail}</Col>                        
+                        <Col xs={4} className='top-data'>Email:</Col>
+                        <Col xs={8} className='ps-1 test-data'>{props.RenterEmail}</Col>
                     </Row>
                     <Row>
-                        <Col xs= {5} className='top-data'>Amount:</Col>
-                        <Col xs= {6} className='test-data'>{props.Amount}</Col>                        
+                        <Col xs={4} className='top-data'>Amount:</Col>
+                        <Col xs={8} className='ps-1 test-data'>{props.Amount}</Col>
                     </Row>
                 </Col>
-                <Col xs={4} className='ms-4 p-0'>
+                <Col xs={5} className='ms-4 p-0'>
                     <Row>
-                        <Col xs= {6} className='test-data-top top-data'>Renter Name:</Col>
-                        <Col xs= {6} className='test-data'>{props.OccupantName}</Col>
-                        
+                        <Col xs={4} className='test-data-top top-data'>Renter Name:</Col>
+                        <Col xs={8} className='ps-3 test-data'>{props.OccupantName}</Col>
+
                     </Row>
                     <Row>
-                        <Col xs= {6} className='test-data-top top-data'>Email:</Col>
-                        <Col xs= {6} className='test-data'>{props.OccupantEmail}</Col> 
+                        <Col xs={4} className='test-data-top top-data'>Email:</Col>
+                        <Col xs={8} className='ps-3 test-data'>{props.OccupantEmail}</Col>
                     </Row>
                 </Col>
-                <Col xs={3} className=''>
-                    <Col xs= {5} className='test-data-top top-data'>Status:</Col>
-                    <Col xs= {5} className='test-data'>{props.Status}</Col> 
+                <Col xs={1} className=''>
+                    <Col xs={5} className='test-data-top top-data'>Status:</Col>
+                    <Col xs={5} className='test-data-amount'>{props.Status}</Col>
                 </Col>
 
             </Row>
