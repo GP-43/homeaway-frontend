6c77c30c9bdd5197ac3d624a7ceeb23fe1ec610a
Tharindu69 <2019cs169@stu.ucsc.cmb.ac.lk>
2022-09-09 19:26:43 +0530

    edited

--- src/components/admin/complaints/AdminComplaintsTable.jsx
+++ src/components/admin/complaints/AdminComplaintsTable.jsx
@@ -51,124 +51,126 @@ function AdminComplaintsTable() {
 
   return (
     <>
-      <Container className="admin-complaint-table-container p-2">
-        <Row className="ms-1 mb-5">
-          <Row className="admin-complaints-table-title">
-            <Col xs={1} className="px-0">
-              <p className="admin-complain-table-row-title">Case No</p>
-            </Col>
-            <Col xs={2} className="px-0">
-              <p className="admin-complain-table-row-title">Complaint Id</p>
-            </Col>
-            <Col xs={2} className="px-0">
-              <p className="admin-complain-table-row-title">Complainer Id</p>
-            </Col>
-            <Col xs={2} className="px-0">
-              <p className="admin-complain-table-row-title">Complainee Id</p>
-            </Col>
-            <Col xs={3} className="px-0">
-              <p className="admin-complain-table-row-title">Subject</p>
-            </Col>
-          </Row>
+      <div className="admin-complaint-main-container">
+        <Container className="admin-complaint-table-container p-2">
+          <Row className="ms-1 mb-5">
+            <Row className="admin-complaints-table-title">
+              <Col xs={1} className="px-0">
+                <p className="admin-complain-table-row-title">Case No</p>
+              </Col>
+              <Col xs={2} className="px-0">
+                <p className="admin-complain-table-row-title">Complaint Id</p>
+              </Col>
+              <Col xs={2} className="px-0">
+                <p className="admin-complain-table-row-title">Complainer Id</p>
+              </Col>
+              <Col xs={2} className="px-0">
+                <p className="admin-complain-table-row-title">Complainee Id</p>
+              </Col>
+              <Col xs={3} className="px-0">
+                <p className="admin-complain-table-row-title">Subject</p>
+              </Col>
+            </Row>
 
-          <Row className="admin-complaint-table-data">
-            {Object.keys(complaints).map((key, index) => (
-              <AdminComplaintsTableRow
-                index={index}
-                caseNo={complaints[index].id}
-                complainerId={complaints[index].compliner_id}
-                complaineeId={complaints[index].complainee_id}
-                subject={complaints[index].subject}
-                status={complaints[index].status}
-                fetchComplatints={fetchComplatints}
-                // Src={i.Src}
-              />
-            ))}
+            <Row className="admin-complaint-table-data">
+              {Object.keys(complaints).map((key, index) => (
+                <AdminComplaintsTableRow
+                  index={index}
+                  caseNo={complaints[index].id}
+                  complainerId={complaints[index].compliner_id}
+                  complaineeId={complaints[index].complainee_id}
+                  subject={complaints[index].subject}
+                  status={complaints[index].status}
+                  fetchComplatints={fetchComplatints}
+                  // Src={i.Src}
+                />
+              ))}
+            </Row>
           </Row>
+        </Container>
+
+        {/* accepted complaints */}
+        <Row className="mt-5">
+          <h4 className="accepted-rejected-titles ms-2">Accepted Complaints</h4>
         </Row>
-      </Container>
+        <Container className="admin-complaint-table-container p-2">
+          <Row className="ms-1 mb-5">
+            <Row className="admin-complaints-table-title">
+              <Col xs={1} className="px-0">
+                <p className="admin-complain-table-row-title">Case No</p>
+              </Col>
+              <Col xs={2} className="px-0">
+                <p className="admin-complain-table-row-title">Complaint Id</p>
+              </Col>
+              <Col xs={2} className="px-0">
+                <p className="admin-complain-table-row-title">Complainer Id</p>
+              </Col>
+              <Col xs={2} className="px-0">
+                <p className="admin-complain-table-row-title">Complainee Id</p>
+              </Col>
+              <Col xs={3} className="px-0">
+                <p className="admin-complain-table-row-title">Subject</p>
+              </Col>
+            </Row>
 
-      {/* accepted complaints */}
-      <Row className="mt-5">
-        <h4 className="accepted-rejected-titles ms-2">Accepted Complaints</h4>
-      </Row>
-      <Container className="admin-complaint-table-container p-2">
-        <Row className="ms-1 mb-5">
-          <Row className="admin-complaints-table-title">
-            <Col xs={1} className="px-0">
-              <p className="admin-complain-table-row-title">Case No</p>
-            </Col>
-            <Col xs={2} className="px-0">
-              <p className="admin-complain-table-row-title">Complaint Id</p>
-            </Col>
-            <Col xs={2} className="px-0">
-              <p className="admin-complain-table-row-title">Complainer Id</p>
-            </Col>
-            <Col xs={2} className="px-0">
-              <p className="admin-complain-table-row-title">Complainee Id</p>
-            </Col>
-            <Col xs={3} className="px-0">
-              <p className="admin-complain-table-row-title">Subject</p>
-            </Col>
+            <Row className="admin-complaint-table-data">
+              {Object.keys(acceptedComplaints).map((key, index) => (
+                <AdminComplaintsTableRow
+                  index={index}
+                  caseNo={acceptedComplaints[index].id}
+                  complainerId={acceptedComplaints[index].compliner_id}
+                  complaineeId={acceptedComplaints[index].complainee_id}
+                  subject={acceptedComplaints[index].subject}
+                  fetchComplatints={fetchComplatints}
+                  // Src={i.Src}
+                />
+              ))}
+            </Row>
           </Row>
+        </Container>
 
-          <Row className="admin-complaint-table-data">
-            {Object.keys(acceptedComplaints).map((key, index) => (
-              <AdminComplaintsTableRow
-                index={index}
-                caseNo={acceptedComplaints[index].id}
-                complainerId={acceptedComplaints[index].compliner_id}
-                complaineeId={acceptedComplaints[index].complainee_id}
-                subject={acceptedComplaints[index].subject}
-                fetchComplatints={fetchComplatints}
-                // Src={i.Src}
-              />
-            ))}
-          </Row>
+        {/* rejected complaints */}
+
+        <Row className="mt-5">
+          <h4 className="accepted-rejected-titles ms-2">Rejected Complaints</h4>
         </Row>
-      </Container>
 
-      {/* rejected complaints */}
+        <Container className="admin-complaint-table-container p-2">
+          <Row className="ms-1 mb-5">
+            <Row className="admin-complaints-table-title">
+              <Col xs={1} className="px-0">
+                <p className="admin-complain-table-row-title">Case No</p>
+              </Col>
+              <Col xs={2} className="px-0">
+                <p className="admin-complain-table-row-title">Complaint Id</p>
+              </Col>
+              <Col xs={2} className="px-0">
+                <p className="admin-complain-table-row-title">Complainer Id</p>
+              </Col>
+              <Col xs={2} className="px-0">
+                <p className="admin-complain-table-row-title">Complainee Id</p>
+              </Col>
+              <Col xs={3} className="px-0">
+                <p className="admin-complain-table-row-title">Subject</p>
+              </Col>
+            </Row>
 
-      <Row className="mt-5">
-        <h4 className="accepted-rejected-titles ms-2">Rejected Complaints</h4>
-      </Row>
-
-      <Container className="admin-complaint-table-container p-2">
-        <Row className="ms-1 mb-5">
-          <Row className="admin-complaints-table-title">
-            <Col xs={1} className="px-0">
-              <p className="admin-complain-table-row-title">Case No</p>
-            </Col>
-            <Col xs={2} className="px-0">
-              <p className="admin-complain-table-row-title">Complaint Id</p>
-            </Col>
-            <Col xs={2} className="px-0">
-              <p className="admin-complain-table-row-title">Complainer Id</p>
-            </Col>
-            <Col xs={2} className="px-0">
-              <p className="admin-complain-table-row-title">Complainee Id</p>
-            </Col>
-            <Col xs={3} className="px-0">
-              <p className="admin-complain-table-row-title">Subject</p>
-            </Col>
+            <Row className="admin-complaint-table-data">
+              {Object.keys(rejectedComplaints).map((key, index) => (
+                <AdminComplaintsTableRow
+                  index={index}
+                  caseNo={rejectedComplaints[index].id}
+                  complainerId={rejectedComplaints[index].compliner_id}
+                  complaineeId={rejectedComplaints[index].complainee_id}
+                  subject={rejectedComplaints[index].subject}
+                  fetchComplatints={fetchComplatints}
+                  // Src={i.Src}
+                />
+              ))}
+            </Row>
           </Row>
-
-          <Row className="admin-complaint-table-data">
-            {Object.keys(rejectedComplaints).map((key, index) => (
-              <AdminComplaintsTableRow
-                index={index}
-                caseNo={rejectedComplaints[index].id}
-                complainerId={rejectedComplaints[index].compliner_id}
-                complaineeId={rejectedComplaints[index].complainee_id}
-                subject={rejectedComplaints[index].subject}
-                fetchComplatints={fetchComplatints}
-                // Src={i.Src}
-              />
-            ))}
-          </Row>
-        </Row>
-      </Container>
+        </Container>
+      </div>
     </>
   );
 }
