6e9df1ebac40be61c9b59035d0e4c69e478afefe
Tharindu69 <2019cs169@stu.ucsc.cmb.ac.lk>
2022-09-09 12:13:45 +0530

    formatted

--- src/components/admin/complaints/AdminComplaintsTableRow.jsx
+++ src/components/admin/complaints/AdminComplaintsTableRow.jsx
@@ -1,57 +1,66 @@
 import React, { useState, useEffect } from "react";
 import axois from "axios";
-import { Col, Row, Container } from 'react-bootstrap';
+import { Col, Row, Container } from "react-bootstrap";
 // for popup
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
 let fetchComplatints;
-const deleteComplain = (complaintId, hideModal) =>{
-    
-    var Id = complaintId;
-    console.log("Delete complaint:",Id);
-    
-    axois
-      .put("http://localhost:4000/admin/reject/complaint/"+Id)
-      .then(() => {
-        console.log("Work");
-        fetchComplatints();
-        hideModal();
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+const deleteComplain = (complaintId, hideModal) => {
+  var Id = complaintId;
+  console.log("Delete complaint:", Id);
 
-}
+  axois
+    .put("http://localhost:4000/admin/reject/complaint/" + Id)
+    .then(() => {
+      console.log("Work");
+      fetchComplatints();
+      hideModal();
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
 //popup function for reject
 function DeletePopup(props) {
-    return (
-      <Modal {...props} size="lg" aria-labelledby="contained-modal-title-vcenter" centered >
-        <Modal.Header closeButton >
-          <Modal.Title id="contained-modal-title-vcenter">
-              Confirm Deletion
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body >
-          <p>Do you want delete? This action cannot be undone.</p>
-        </Modal.Body>
-        <Modal.Footer >
-            <Button className='confirm-delete btn-danger' onClick={()=>deleteComplain(props.complaintId, props.onHide)}>Reject</Button>
-          <Button onClick={props.onHide}>Cancel</Button>
-        </Modal.Footer>
-      </Modal>
-    );
-  }
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Confirm Deletion
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p>Do you want delete? This action cannot be undone.</p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button
+          className="confirm-delete btn-danger"
+          onClick={() => deleteComplain(props.complaintId, props.onHide)}
+        >
+          Reject
+        </Button>
+        <Button onClick={props.onHide}>Cancel</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
 
 // accept complaint
 
-function acceptComplaint(complaintId){
+function acceptComplaint(complaintId, complainerId, complaineeId) {
   var Id = complaintId;
-  console.log("Accept complaint:",Id);
-  
+  console.log("Accept complaint:", Id, complaineeId, complainerId);
+
+
   axois
-    .put("http://localhost:4000/admin/accept/complaint/"+Id)
+    .put("http://localhost:4000/admin/accept/complaint/" + Id)
     .then(() => {
       console.log("Work");
       fetchComplatints();
@@ -62,49 +71,62 @@ function acceptComplaint(complaintId){
 }
 
 function AdminComplaintsTableRow(props) {
-
   fetchComplatints = props.fetchComplatints;
 
-    const Id = props.caseNo;
+  const Id = props.caseNo;
 
-    const [modalShow, setModalShow] = React.useState(false);
+  const [modalShow, setModalShow] = React.useState(false);
 
-    return (
-        <Row className='admin-complain-table-row mx-0 px-0'>
-            <Col xs={1} className="px-0">
-                <p className='admin-complain-table-row-text'>{props.caseNo}</p>
-            </Col>
-            <Col xs={1} className="px-0">
-                <p className='admin-complain-table-row-text'>{props.index + 1}</p>
-            </Col>
-            <Col xs={2} className="px-0">
-                <p className='admin-complain-table-row-text'>{props.subject}</p>
-            </Col>
-            <Col xs={1} className="px-0">
-                <p className='admin-complain-table-row-text'>{props.complainerId}</p></Col>
-            <Col xs={2} className="px-0">
-                <p className='admin-complain-table-row-text'>{props.complaineeId}</p></Col>
-            {/* <Col xs={3} className="px-0">
+  return (
+    <Row className="admin-complain-table-row mx-0 px-0">
+      <Col xs={1} className="px-0">
+        <p className="admin-complain-table-row-text">{props.caseNo}</p>
+      </Col>
+      <Col xs={1} className="px-0">
+        <p className="admin-complain-table-row-text">{props.index + 1}</p>
+      </Col>
+      <Col xs={2} className="px-0">
+        <p className="admin-complain-table-row-text">{props.subject}</p>
+      </Col>
+      <Col xs={1} className="px-0">
+        <p className="admin-complain-table-row-text">{props.complainerId}</p>
+      </Col>
+      <Col xs={2} className="px-0">
+        <p className="admin-complain-table-row-text">{props.complaineeId}</p>
+      </Col>
+      {/* <Col xs={3} className="px-0">
                 <p className='admin-complain-table-row-text'>{props.senderEmail}</p></Col>
             <Col xs={2} className="px-0">
                 <p className='admin-complain-table-row-text'>{props.date} <br /> {props.time}</p></Col> */}
-            <Col xs={1} className="px-0">
-                <button className='admin-complaint-accept-btn px-3 pb-0' variant="primary" onClick={()=>acceptComplaint(Id)}>
-                    <p>ACCEPT</p>
-                </button>
-                
-            </Col>
-            <Col xs={1} className="px-0">
-                <button className='admin-complaint-delete-btn px-3 pb-0' variant="primary" onClick={() =>
-                {
-                    setModalShow(true)
-                }}>
-                    <p>REJECT</p>
-                </button>
-                
-                <DeletePopup show={modalShow} onHide={() => setModalShow(false)} complaintId={Id}/>
-            </Col>
-        </Row>
-    ); 
+      <Col xs={1} className="px-0">
+        <button
+          className="admin-complaint-accept-btn px-3 pb-0"
+          variant="primary"
+          onClick={() =>
+            acceptComplaint(Id, props.complainerId, props.complaineeId)
+          }
+        >
+          <p>ACCEPT</p>
+        </button>
+      </Col>
+      <Col xs={1} className="px-0">
+        <button
+          className="admin-complaint-delete-btn px-3 pb-0"
+          variant="primary"
+          onClick={() => {
+            setModalShow(true);
+          }}
+        >
+          <p>REJECT</p>
+        </button>
+
+        <DeletePopup
+          show={modalShow}
+          onHide={() => setModalShow(false)}
+          complaintId={Id}
+        />
+      </Col>
+    </Row>
+  );
 }
-export default AdminComplaintsTableRow
+export default AdminComplaintsTableRow;
