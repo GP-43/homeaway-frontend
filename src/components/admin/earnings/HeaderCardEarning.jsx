2384aa9a0a49d2070feb9a658c2341e220f49e2e
Tharaki-99 <dilshanitharaki@gmail.com>
2022-10-22 22:42:50 +0530

    done

---
+++ src/components/admin/earnings/HeaderCardEarning.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Card, Col, Container, Row } from "react-bootstrap";
+import { FaHome } from "react-icons/fa";
+import { FaUserCircle } from "react-icons/fa";
+import { FiLogOut } from "react-icons/fi";
+import Button from "react-bootstrap/Button";
+
+function HeaderCardOccupant() {
+    const handleOnLogoClick = () => {
+        window.location.replace("/");
+        sessionStorage.removeItem("accessToken");
+    }
+    return (
+        <Card className="Header-card">
+            <Col className="d-flex flex-row  Header-card-div">
+                <a href="" className="Header-card-div-text ms-2 mt-2">
+                    <FaHome className="header-card-home-icon " size="lg" />
+                </a>
+                <h6 className="Header-card-div-text ">/</h6>
+                <a href="" className="Header-card-div-text ">
+                    <h6 className="Header-card-div-text ms-0 mt-0">Earning</h6>
+                </a>
+            </Col>
+            <Row>
+                <Col xs={10} className="Header-card-div">
+                    <h4 className="Header-card-div-text">Earning</h4>
+                </Col>
+                <Col xs={1} className="user-icon-box"><FiLogOut className="user-icon" /></Col>
+                <Col xs={1} className="admin-name"><Button className='bg-transparent border-0 py-0'
+                    onClick={handleOnLogoClick}>Logout</Button></Col>
+            </Row>
+        </Card>
+    );
+}
+
+export default HeaderCardOccupant;
--- src/components/admin/navbar/LeftNavigation.jsx
+++ src/components/admin/navbar/LeftNavigation.jsx
@@ -1,11 +1,12 @@
-import React, {useEffect, useState} from "react";
-import {Col, Image} from "react-bootstrap";
-import {Link, useLocation, useNavigate} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Col, Image } from "react-bootstrap";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import Logo from "../../../assets/images/logo/logo.png"
-import {FiUsers} from "react-icons/fi";
-import { MdDashboard, MdOutlinePayments,MdReport } from "react-icons/md";
+import { FiUsers } from "react-icons/fi";
+import { MdDashboard, MdOutlinePayments, MdReport } from "react-icons/md";
 import { FaHouseUser } from "react-icons/fa";
 import { FaHome } from "react-icons/fa";
+import { FaMoneyBill } from "react-icons/fa";
 
 function LeftNavigation() {
 
@@ -25,49 +26,55 @@ function LeftNavigation() {
     return (
         <Col className='left-navigation p-1'>
             <div className='h-100'>
-            <div className='px-lg-4 pt-lg-5 w-100 d-flex justify-content-center'>
-                <Image src={Logo} className='logo bg-transparent' alt='LOGO' onClick={handleOnLogoClick}/>
-            </div>
-            <div className=' d-flex align-items-center h-75'>
-                <ul className='list-unstyled px-2 nav-list bg-transparent w-100'>
-                    <Link to='/admin/dashboard' className={url === '/admin/dashboard' ? 'active mb-2' : 'mb-2'}>
-                        <li className='px-lg-3 bg-transparent d-flex align-items-center'>
-                            <MdDashboard className='bg-transparent me-3'/>
-                            Dashboard
-                        </li>
-                    </Link>
-                    <Link to='/admin/occupants' className={url === '/admin/occupants' ? 'active mb-2' : 'mb-2'}>
-                        <li className='px-lg-3 bg-transparent d-flex align-items-center'>
-                            <FiUsers className='bg-transparent me-3'/>
-                            Occupants
-                        </li>
-                    </Link>
-                    <Link to='/admin/renters' className={url === '/admin/renters' ? 'active mb-2' : 'mb-2'}>
-                        <li className='px-lg-3 bg-transparent d-flex align-items-center'>
-                            <FaHouseUser className='bg-transparent me-3'/>
-                            Renters
-                        </li>
-                    </Link>
-                    <Link to='/admin/payment' className={url === '/admin/payment' ? 'active mb-2' : 'mb-2'}>
-                        <li className='px-lg-3 bg-transparent d-flex align-items-center'>
-                            <MdOutlinePayments className='bg-transparent me-3'/>
-                            Payment
-                        </li>
-                    </Link>
-                    <Link to='/admin/places' className={url === '/admin/places' ? 'active mb-2' : 'mb-2'}>
-                        <li className='px-lg-3 bg-transparent d-flex align-items-center'>
-                            <FaHome className='bg-transparent me-3'/>
-                            Places
-                        </li>
-                    </Link>
-                    <Link to='/admin/complains' className={url === '/admin/complains' ? 'active mb-2' : 'mb-2'}>
-                        <li className='px-lg-3 bg-transparent d-flex align-items-center'>
-                            <MdReport className='bg-transparent me-3'/>
-                            Complaints
-                        </li>
-                    </Link>
-                </ul>
-            </div>
+                <div className='px-lg-4 pt-lg-5 w-100 d-flex justify-content-center'>
+                    <Image src={Logo} className='logo bg-transparent' alt='LOGO' onClick={handleOnLogoClick} />
+                </div>
+                <div className=' d-flex align-items-center h-75'>
+                    <ul className='list-unstyled px-2 nav-list bg-transparent w-100'>
+                        <Link to='/admin/dashboard' className={url === '/admin/dashboard' ? 'active mb-2' : 'mb-2'}>
+                            <li className='px-lg-3 bg-transparent d-flex align-items-center'>
+                                <MdDashboard className='bg-transparent me-3' />
+                                Dashboard
+                            </li>
+                        </Link>
+                        <Link to='/admin/occupants' className={url === '/admin/occupants' ? 'active mb-2' : 'mb-2'}>
+                            <li className='px-lg-3 bg-transparent d-flex align-items-center'>
+                                <FiUsers className='bg-transparent me-3' />
+                                Occupants
+                            </li>
+                        </Link>
+                        <Link to='/admin/renters' className={url === '/admin/renters' ? 'active mb-2' : 'mb-2'}>
+                            <li className='px-lg-3 bg-transparent d-flex align-items-center'>
+                                <FaHouseUser className='bg-transparent me-3' />
+                                Renters
+                            </li>
+                        </Link>
+                        <Link to='/admin/payment' className={url === '/admin/payment' ? 'active mb-2' : 'mb-2'}>
+                            <li className='px-lg-3 bg-transparent d-flex align-items-center'>
+                                <MdOutlinePayments className='bg-transparent me-3' />
+                                Payment
+                            </li>
+                        </Link>
+                        <Link to='/admin/places' className={url === '/admin/places' ? 'active mb-2' : 'mb-2'}>
+                            <li className='px-lg-3 bg-transparent d-flex align-items-center'>
+                                <FaHome className='bg-transparent me-3' />
+                                Places
+                            </li>
+                        </Link>
+                        <Link to='/admin/earnings' className={url === '/admin/earnings' ? 'active mb-2' : 'mb-2'}>
+                            <li className='px-lg-3 bg-transparent d-flex align-items-center'>
+                                <FaMoneyBill className='bg-transparent me-3' />
+                                Earnings
+                            </li>
+                        </Link>
+                        <Link to='/admin/complains' className={url === '/admin/complains' ? 'active mb-2' : 'mb-2'}>
+                            <li className='px-lg-3 bg-transparent d-flex align-items-center'>
+                                <MdReport className='bg-transparent me-3' />
+                                Complaints
+                            </li>
+                        </Link>
+                    </ul>
+                </div>
             </div>
         </Col>
     );
