28c2233dfcd5488de498c4d4aeacb99eecdefe63
Semini48 <84171007+Semini48@users.noreply.github.com>
2022-10-25 00:15:27 +0530

    done

--- src/components/user/paymentbox/PaymentBox.jsx
+++ src/components/user/paymentbox/PaymentBox.jsx
@@ -1,50 +1,44 @@
 import React, { useState, useEffect } from "react";
-import { Container, Col, Row } from "react-bootstrap";
+import { Container, Col, Row, Button } from "react-bootstrap";
+import {Routes, Route, useNavigate} from 'react-router-dom';
 import axois from "axios";
 
 function PaymentBox() {
-  const [payments, setPayments] = useState([]);
+  const navigate = useNavigate();
 
-    useEffect((event) => {
-      axois
-      .get("http://localhost:4000/renter/transactions")
+  const navigateToContacts = () => {
+    navigate('/user/paymentofplaces');
+  };
+  
+  const userDetails = JSON.parse(sessionStorage.getItem('accessToken'));
+  const userId = userDetails.userId;
+
+  //console.log(userDetails);
+  const [payment, setPayments] = useState([]);
+
+useEffect((event) => {
+  axois
+      .get("http://localhost:4000/renter/transactions/" + userId)
       .then((data) => {
-          const detail = data.data;
-          setPayments(detail);
-          console.log(detail);
+        const payment = data.data;
+        setPayments({ ...payment });
+        console.log(payment[0]?.total_amount);
       })
       .catch((error) => {
-          console.log(error);
+        console.log(error);
       });
-    }, []);
-// console.log(payments[0].total_amount);
+  }, []);
 
-  // useEffect((event) => {
-  //   axois
-  //     .get("http://localhost:4000/renter/transactions")
-  //     .then((res) => {
-  //       console.log(res.data[0].amount)
-  //       setPayments(res.data)
-  //     })
-  //     .catch((err) => {
-  //       console.log(err)
-  //     });
-  // }, []);
-  //   console.log(payments);
   return (
     <Container className="payment-box-container pt-1">
-      <Col xs={10} className="p-4 mx-2 my-3 payment-box">
+      <Col xs={10} className="p-4 mx-2 my-2 payment-box">
         <Row>
           <h7 xs={10} className="px-2 heading ">
             Total Received
           </h7>
-          <dev className="px-2 received-amount">
+          <dev className="px-2 received-amount">LKR {payment[0]?.total_amount}
           </dev>
-          <dev className="px-2 sen">+10% since last two weeks</dev>
-          <h7 xs={10} className="px-2 pt-4 sub-heading ">
-            Pending
-          </h7>
-          <dev className="px-2 pending-amount">LKR 10560.00</dev>
+          <Button className="px-2 more-btn" onClick={navigateToContacts}>Click here for a detailed version..</Button>
         </Row>
       </Col>
     </Container>
