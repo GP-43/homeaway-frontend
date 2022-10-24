7c6c254f68cacdfb01afc4269cbf12031f4dc8af
Tharindu69 <2019cs169@stu.ucsc.cmb.ac.lk>
2022-10-24 19:54:18 +0530

    still working

--- src/views/pages/user/UserProfile.jsx
+++ src/views/pages/user/UserProfile.jsx
@@ -1,126 +1,183 @@
 import React from "react";
-import {Col, Row, Form} from "react-bootstrap";
+import { Col, Row, Form } from "react-bootstrap";
 import icon1 from "../../../assets/images/profile/Vector.png";
-import {useState} from "react";
-import Button from 'react-bootstrap/Button';
-import {FaStar, FaEdit} from "react-icons/fa";
-import {AiOutlineFileDone} from "react-icons/ai";
+import { useState, useEffect } from "react";
+import Button from "react-bootstrap/Button";
+import { FaStar, FaEdit } from "react-icons/fa";
+import { AiOutlineFileDone } from "react-icons/ai";
+import axois from "axios";
+const base_url = process.env.REACT_APP_BASE_URL;
 
 function UserProfile() {
-    const [showPassword, setShowPassword] = useState(false);
-    const [name, setName] = useState("Thushan Dananjaya");
-    const [email, setEmail] = useState("Thushan@gmail.com");
-    const [location, setLocation] = useState("Gampaha");
-    const [contact, setContact] = useState("0716113769");
-    const [password, setPassword] = useState("Thushan123#");
+  //get id from session
+  const userDetails = JSON.parse(sessionStorage.getItem("accessToken"));
+  const userId = userDetails.userId;
 
-    const [isNameDisabled, setIsNameDisabled] = useState(true);
-    const [isFormDisabled, setIsFormDisabled] = useState(true);
+  const [profileDetails, setProfileDetails] = useState({});
+  const [showPassword, setShowPassword] = useState(false);
+  const [name, setName] = useState("Thushan Dananjaya");
+  const [email, setEmail] = useState("Thushan@gmail.com");
+  const [location, setLocation] = useState("Gampaha");
+  const [contact, setContact] = useState("0716113769");
+  const [password, setPassword] = useState("Thushan123#");
 
-    const handleOnSubmit = () => {
-        setIsFormDisabled(true);
-    }
+  const [isNameDisabled, setIsNameDisabled] = useState(true);
+  const [isFormDisabled, setIsFormDisabled] = useState(true);
 
-    const handleOnNameSubmit = () => {
-        setIsNameDisabled(true);
-    }
+  const handleOnSubmit = () => {
+    setIsFormDisabled(true);
+  };
 
-    return (
-        <>
-            <Row>
-                <Col className="profile-picture ms-5 mt-3 mb-3" lg={3}>
-                    <img src={icon1} alt="" className="profile-photo-edit-icon"/>
-                </Col>
-                <Col className="profile-name-rate-container">
-                    <Row className="profile-name-container">
-                        <Col lg={8} className='p-0'>
-                            <Form.Group className="mb-3">
-                                <Form.Control placeholder="" value={name} disabled={isNameDisabled}
-                                              onChange={(e) => setName(e.target.value)}/>
-                            </Form.Group>
-                            {/*  <input*/}
-                            {/*  type="text"*/}
-                            {/*  value={name}*/}
-                            {/*  onChange={(e) => setName(e.target.value)}*/}
-                            {/*/>*/}
+  const handleOnNameSubmit = () => {
+    setIsNameDisabled(true);
+  };
 
-                        </Col>
-                        <Col lg={4} className='p-0'>
-                            <h3>
-                            <FaEdit className="ms-3 edit-icon" onClick={() => setIsNameDisabled(false)}/>
-                            <AiOutlineFileDone className=" ms-2 done-icon" onClick={handleOnNameSubmit}/>
-                            </h3>
-                        </Col>
-                        <hr className="profile-name-underline"/>
-                    </Row>
-                    <Row>
-                        <h4><FaStar className="mb-2 me-3"/>3.5</h4>
-                    </Row>
-                </Col>
-            </Row>
+  // refresh table
+  function fetchProfileDetails() {
+    // get complaints
+    axois
+      .get("http://localhost:4000/admin/reject/complaint/" + userId)
+      .then((data) => {
+        const profileDetails = data.data;
+        setProfileDetails({ ...profileDetails });
+        //setoccupantData(false)
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
-            <Col>
-                <Row className="m-3">
-                    <Col lg={4}>
-                        <h5>Email :</h5>
-                    </Col>
-                    <Col lg={8} className="p-0">
-                        <Form.Group className="mb-3">
-                            <Form.Control placeholder="" value={email} disabled={true}/>
-                        </Form.Group>
-                    </Col>
-                </Row>
-                <Row className="m-3">
-                    <Col lg={4}>
-                        <h5>Location :</h5>
-                    </Col>
-                    <Col lg={8} className='p-0'>
-                        <Form.Group className="mb-3">
-                            <Form.Control placeholder="" value={location} disabled={isFormDisabled}
-                                          onChange={(e) => setLocation(e.target.value)}/>
-                        </Form.Group>
-                    </Col>
-                </Row>
-                <Row className="m-3">
-                    <Col lg={4}>
-                        <h5>Contact No :</h5>
-                    </Col>
-                    <Col lg={8} className="p-0">
-                        <Form.Group className="mb-3">
-                            <Form.Control placeholder="" value={contact} disabled={isFormDisabled}
-                                          onChange={(e) => setContact(e.target.value)}/>
-                        </Form.Group>
-                    </Col>
-                </Row>
-                <Row className="m-3">
-                    <Col lg={4}>
-                        <h5>Password :</h5>
-                    </Col>
-                    <Col lg={8} className="p-0">
-                        <Form.Group className="mb-3">
-                            <Form.Control placeholder="" value={password} disabled={isFormDisabled}
-                                          onChange={(e) => setPassword(e.target.value)}
-                                          type={showPassword ? "text" : "password"}/>
-                            <input
-                                type="checkbox"
-                                checked={showPassword}
-                                onChange={(e) => setShowPassword(e.target.checked)}
-                            /> show password
-                        </Form.Group>
+  useEffect((event) => {
+    fetchProfileDetails();
+  }, []);
 
-
-                    </Col>
-                </Row>
-                <Row className="justify-content-end">
-                    <Button variant="warning" className="profile-edit-button m-3"
-                            onClick={() => setIsFormDisabled(false)}>Edit</Button>
-                    <Button className="profile-submit-button m-3" onClick={handleOnSubmit}>Submit</Button>
-                </Row>
+  return (
+    <>
+      <Row>
+        <Col className="profile-picture ms-5 mt-3 mb-3" lg={3}>
+          <img src={icon1} alt="" className="profile-photo-edit-icon" />
+        </Col>
+        <Col className="profile-name-rate-container">
+          <Row className="profile-name-container">
+            <Col lg={8} className="p-0">
+              <Form.Group className="mb-3">
+                <Form.Control
+                  placeholder=""
+                  value={name}
+                  disabled={isNameDisabled}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </Form.Group>
+              {/*  <input*/}
+              {/*  type="text"*/}
+              {/*  value={name}*/}
+              {/*  onChange={(e) => setName(e.target.value)}*/}
+              {/*/>*/}
             </Col>
-        </>
-    );
+            <Col lg={4} className="p-0">
+              <h3>
+                <FaEdit
+                  className="ms-3 edit-icon"
+                  onClick={() => setIsNameDisabled(false)}
+                />
+                <AiOutlineFileDone
+                  className=" ms-2 done-icon"
+                  onClick={handleOnNameSubmit}
+                />
+              </h3>
+            </Col>
+            <hr className="profile-name-underline" />
+          </Row>
+          <Row>
+            <h4>
+              <FaStar className="mb-2 me-3" />
+              3.5
+            </h4>
+          </Row>
+        </Col>
+      </Row>
+
+      <Col>
+        <Row className="m-3">
+          <Col lg={4}>
+            <h5>Email :</h5>
+          </Col>
+          <Col lg={8} className="p-0">
+            <Form.Group className="mb-3">
+              <Form.Control placeholder="" value={email} disabled={true} />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row className="m-3">
+          <Col lg={4}>
+            <h5>Location :</h5>
+          </Col>
+          <Col lg={8} className="p-0">
+            <Form.Group className="mb-3">
+              <Form.Control
+                placeholder=""
+                value={location}
+                disabled={isFormDisabled}
+                onChange={(e) => setLocation(e.target.value)}
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row className="m-3">
+          <Col lg={4}>
+            <h5>Contact No :</h5>
+          </Col>
+          <Col lg={8} className="p-0">
+            <Form.Group className="mb-3">
+              <Form.Control
+                placeholder=""
+                value={contact}
+                disabled={isFormDisabled}
+                onChange={(e) => setContact(e.target.value)}
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row className="m-3">
+          <Col lg={4}>
+            <h5>Password :</h5>
+          </Col>
+          <Col lg={8} className="p-0">
+            <Form.Group className="mb-3">
+              <Form.Control
+                placeholder=""
+                value={password}
+                disabled={isFormDisabled}
+                onChange={(e) => setPassword(e.target.value)}
+                type={showPassword ? "text" : "password"}
+              />
+              <input
+                type="checkbox"
+                checked={showPassword}
+                onChange={(e) => setShowPassword(e.target.checked)}
+              />{" "}
+              show password
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row className="justify-content-end">
+          <Button
+            variant="warning"
+            className="profile-edit-button m-3"
+            onClick={() => setIsFormDisabled(false)}
+          >
+            Edit
+          </Button>
+          <Button
+            className="profile-submit-button m-3"
+            onClick={handleOnSubmit}
+          >
+            Submit
+          </Button>
+        </Row>
+      </Col>
+    </>
+  );
 }
 
 export default UserProfile;
-
-
