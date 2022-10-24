4e73eb4f5e040908fbbaf5ac26c91362e871b905
Tharindu69 <2019cs169@stu.ucsc.cmb.ac.lk>
2022-10-24 09:38:54 +0530

    design implemented, need to implement hooks part

--- src/views/pages/user/UserProfile.jsx
+++ src/views/pages/user/UserProfile.jsx
@@ -1,74 +1,85 @@
-import React from 'react';
-import {Col, Row} from "react-bootstrap";
-import icon1 from "../../../assets/images/profile/Vector.png"
-
-//show password function
-function showPassword() {
-    var x = document.getElementById("showPassword1");
-    if (x.type === "password") {
-      x.type = "text";
-    } else {
-      x.type = "password";
-    }
-  }
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import icon1 from "../../../assets/images/profile/Vector.png";
+import { useState } from "react";
+import Button from 'react-bootstrap/Button';
+import { FaStar, FaEdit } from "react-icons/fa";
 
 function UserProfile() {
-    return (
-        <>
-        <Row>
-            <Col className='profile-picture ms-5 mt-3 mb-3' lg = {3}>
-             <img src={icon1} alt="" />    
-            </Col>
-            <Col>
-            <Row>
-                <h1>
-                    Thushan Dananjaya
-                </h1>
-            </Row>
-            <Row>
-                <h4>
-                    3.5 
-                </h4>
-            </Row>
-            </Col>
-        </Row>
-
-        <Col>
-        <Row className='m-3'>
-            <Col lg = {4}>
-                <h5>Email :</h5>
-            </Col>
-            <Col lg = {8} className="profile-data-fieled">
-                <input type="text" value="Thushan Dananjaya" className='profile-data-fieled-input'/>
-            </Col>
-        </Row>
-        <Row className='m-3'>
-            <Col lg = {4}>
-                <h5>Location :</h5>
-            </Col>
-            <Col lg = {8} >
-
-            </Col>
-        </Row>
-        <Row className='m-3'>
-            <Col lg = {4}>
-                <h5>Contact No :</h5></Col>
-            <Col lg = {8} className="profile-data-fieled">
-                <input type="text" value="0781102052" className='profile-data-fieled-input'/>
-            </Col>
-        </Row>
-        <Row className='m-3'>
-            <Col lg = {4}>
-                <h5>Password :</h5></Col>
-            <Col lg = {8} className="profile-data-fieled">
-                <input type="password" value="Thushan12345@" className='profile-data-fieled-input' id='showPassword1'/>
-                
-               <input type="checkbox" onclick="showPassword()" /> show password
-            </Col>
-        </Row>
+  const [showPassword, setShowPassword] = useState(false);
+  return (
+    <>
+      <Row>
+        <Col className="profile-picture ms-5 mt-3 mb-3" lg={3}>
+          <img src={icon1} alt="" className="profile-photo-edit-icon"/>
         </Col>
-        </>
-    );
+        <Col className="profile-name-rate-container">
+          <Row className="profile-name-container">
+            <h1>Thushan Dananjaya <FaEdit className="mb-2 ms-5"/></h1>
+            <hr className="profile-name-underline"/>
+          </Row>
+          <Row>
+            <h4><FaStar className="mb-2 me-3"/>3.5</h4>
+          </Row>
+        </Col>
+      </Row>
+
+      <Col>
+        <Row className="m-3">
+          <Col lg={4}>
+            <h5>Email :</h5>
+          </Col>
+          <Col lg={8} className="profile-data-fieled">
+            <input
+              type="text"
+              value="Thushan Dananjaya"
+              className="profile-data-fieled-input"
+            />
+          </Col>
+        </Row>
+        <Row className="m-3">
+          <Col lg={4}>
+            <h5>Location :</h5>
+          </Col>
+          <Col lg={8}></Col>
+        </Row>
+        <Row className="m-3">
+          <Col lg={4}>
+            <h5>Contact No :</h5>
+          </Col>
+          <Col lg={8} className="profile-data-fieled">
+            <input
+              type="text"
+              value="0781102052"
+              className="profile-data-fieled-input"
+            />
+          </Col>
+        </Row>
+        <Row className="m-3">
+          <Col lg={4}>
+            <h5>Password :</h5>
+          </Col>
+          <Col lg={8} className="profile-data-fieled">
+            <input
+              type={showPassword ? "text" : "password"}
+              value="Thushan12345@"
+              className="profile-data-fieled-input"
+            />
+            <input
+              type="checkbox"
+              checked={showPassword}
+              onChange={(e) => setShowPassword(e.target.checked)}
+            />{" "}
+            show password
+          </Col>
+        </Row>
+        <Row className="justify-content-end">
+            <Button variant="warning" className="profile-edit-button m-3">Edit</Button>
+            <Button className="profile-submit-button m-3">Submit</Button>
+        </Row>
+      </Col>
+    </>
+  );
 }
 
 export default UserProfile;
