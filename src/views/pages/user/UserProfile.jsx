222705a99121459b2b6c08fa8822f26f64beab7f
Tharindu69 <2019cs169@stu.ucsc.cmb.ac.lk>
2022-10-24 08:25:41 +0530

    need to go another branch

--- src/views/pages/user/UserProfile.jsx
+++ src/views/pages/user/UserProfile.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import {Col, Row} from "react-bootstrap";
 import icon1 from "../../../assets/images/profile/Vector.png"
+
+//show password function
+function showPassword() {
+    var x = document.getElementById("showPassword1");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
+
 function UserProfile() {
     return (
         <>
@@ -28,7 +39,7 @@ function UserProfile() {
                 <h5>Email :</h5>
             </Col>
             <Col lg = {8} className="profile-data-fieled">
-
+                <input type="text" value="Thushan Dananjaya" className='profile-data-fieled-input'/>
             </Col>
         </Row>
         <Row className='m-3'>
@@ -43,14 +54,16 @@ function UserProfile() {
             <Col lg = {4}>
                 <h5>Contact No :</h5></Col>
             <Col lg = {8} className="profile-data-fieled">
-
+                <input type="text" value="0781102052" className='profile-data-fieled-input'/>
             </Col>
         </Row>
         <Row className='m-3'>
             <Col lg = {4}>
                 <h5>Password :</h5></Col>
             <Col lg = {8} className="profile-data-fieled">
-
+                <input type="password" value="Thushan12345@" className='profile-data-fieled-input' id='showPassword1'/>
+                
+               <input type="checkbox" onclick="showPassword()" /> show password
             </Col>
         </Row>
         </Col>
