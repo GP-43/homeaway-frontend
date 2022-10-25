62aafd53ec2cf293323748a1d100f102d25de023
Thushan Dananjana <62166556+Thushan98@users.noreply.github.com>
2022-10-25 23:48:10 +0530

    phone number ok

--- src/views/pages/anons/Signup.jsx
+++ src/views/pages/anons/Signup.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import {Button, Col, Container, Form, InputGroup, Row} from "react-bootstrap";
-import {Link, useNavigate} from "react-router-dom";
-import {FiLock, FiMail, FiPhone, FiUser} from "react-icons/fi";
-import {GrLocation} from "react-icons/gr";
+import { Button, Col, Container, Form, InputGroup, Row } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
+import { FiLock, FiMail, FiPhone, FiUser } from "react-icons/fi";
+import { GrLocation } from "react-icons/gr";
 import axios from "axios";
 import * as yup from 'yup';
-import {Formik, ErrorMessage} from 'formik';
+import { Formik, ErrorMessage } from 'formik';
 
 function Signup() {
 
@@ -19,7 +19,7 @@ function Signup() {
                 <Col md={5} className='animation-shield d-flex align-items-center justify-content-center'>
                     <div>
                         <h1 className='mb-3'>Welcome Back!</h1>
-                        <p className='mb-4'>To keep connect with us please<br/>login with your personal details</p>
+                        <p className='mb-4'>To keep connect with us please<br />login with your personal details</p>
                         <Link to={'/login'}>
                             <Button type="button" className="btn btn-outline-light">SIGN IN</Button>
                         </Link>
@@ -40,8 +40,8 @@ function Signup() {
                             name: yup.string().required()
                                 .max(50, 'Must be 50 characters or less')
                                 .min(1, 'Must be 1 characters or more'),
-                            contact: yup.string().required(),
-                                // .matches(phoneRegExp, 'Phone number format: +{country code}-{number}'),
+                            contact: yup.string().matches(phoneRegExp, 'Phone number is not valid').required(),
+                            // .matches(phoneRegExp, 'Phone number format: +{country code}-{number}'),
                             location: yup.string().required(),
                             password: yup.string().required()
                                 .max(25, 'Must be 25 characters or less')
@@ -52,10 +52,10 @@ function Signup() {
                         onSubmit={(values) => {
                             axios.post("http://localhost:4000/auth/signup", values).then((response) => {
                                 console.log(response);
-                                if(response.data == "SUCCESS"){
+                                if (response.data == "SUCCESS") {
                                     navigate('/login');
                                     alert('User Registred successfully!');
-                                }else {
+                                } else {
                                     alert('User already Exists!');
                                 }
                             });
@@ -71,14 +71,14 @@ function Signup() {
                                     <ErrorMessage name='existEmail' component='div' className='error-msg' />
                                     <InputGroup className="my-3 data-field" id="validationFormik01" hasValidation>
                                         <InputGroup.Text className='data-field-icon'
-                                                         id="basic-addon1"><FiMail/></InputGroup.Text>
+                                            id="basic-addon1"><FiMail /></InputGroup.Text>
                                         <Form.Control type="email"
-                                                      placeholder="Email"
-                                                      aria-describedby="inputGroupPrepend"
-                                                      name="email"
+                                            placeholder="Email"
+                                            aria-describedby="inputGroupPrepend"
+                                            name="email"
                                             // onChange={handleChange}
                                             // value={values.email}
-                                                      {...formik.getFieldProps("email")}
+                                            {...formik.getFieldProps("email")}
                                         />
                                         {/*<Form.Control.Feedback type="invalid">*/}
                                         {/*    {errors.email}*/}
@@ -87,16 +87,16 @@ function Signup() {
                                     <ErrorMessage name='email' component='div' className='error-msg' />
                                     <InputGroup className="mb-3 data-field" id="formBasicName">
                                         <InputGroup.Text className='data-field-icon'
-                                                         id="basic-addon1"><FiUser/></InputGroup.Text>
+                                            id="basic-addon1"><FiUser /></InputGroup.Text>
                                         <Form.Control type="text"
-                                                      placeholder="Name"
-                                                      aria-describedby="inputGroupPrepend"
-                                                      name="name"
+                                            placeholder="Name"
+                                            aria-describedby="inputGroupPrepend"
+                                            name="name"
                                             // onChange={handleChange}
                                             // onBlur={handleBlur}
                                             // value={values.name}
                                             // isInvalid={!!errors.name}
-                                                      {...formik.getFieldProps("name")}
+                                            {...formik.getFieldProps("name")}
                                         />
                                         {/*<Form.Control.Feedback type="invalid">*/}
                                         {/*    {errors.name}*/}
@@ -105,16 +105,16 @@ function Signup() {
                                     <ErrorMessage name='name' component='div' className='error-msg' />
                                     <InputGroup className="mb-3 data-field" id="formBasicContact">
                                         <InputGroup.Text className='data-field-icon'
-                                                         id="basic-addon1"><FiPhone/></InputGroup.Text>
+                                            id="basic-addon1"><FiPhone /></InputGroup.Text>
                                         <Form.Control type="text"
-                                                      placeholder="Contact No"
-                                                      aria-describedby="inputGroupPrepend"
-                                                      name="contact"
+                                            placeholder="Contact No"
+                                            aria-describedby="inputGroupPrepend"
+                                            name="contact"
                                             // onChange={handleChange}
                                             // onBlur={handleBlur}
                                             // value={values.contact}
                                             // isInvalid={!!errors.contact}
-                                                      {...formik.getFieldProps("contact")}
+                                            {...formik.getFieldProps("contact")}
                                         />
                                         {/*<Form.Control.Feedback type="invalid">*/}
                                         {/*    {errors.contact}*/}
@@ -124,12 +124,12 @@ function Signup() {
                                     {/*location input need to go here. have to implement*/}
                                     <InputGroup className="mb-3 data-field" id="formBasicLocation">
                                         <InputGroup.Text className='data-field-icon'
-                                                         id="basic-addon1"><GrLocation/></InputGroup.Text>
+                                            id="basic-addon1"><GrLocation /></InputGroup.Text>
                                         <Form.Control type="text" placeholder="Location"
                                             // onChange={handleChange}
-                                                      aria-describedby="inputGroupPrepend"
-                                                      name="location"
-                                                      {...formik.getFieldProps("location")}
+                                            aria-describedby="inputGroupPrepend"
+                                            name="location"
+                                            {...formik.getFieldProps("location")}
 
                                         />
                                         {/*<Form.Control.Feedback type="invalid">*/}
@@ -139,13 +139,13 @@ function Signup() {
                                     <ErrorMessage name='location' component='div' className='error-msg' />
                                     <InputGroup className="mb-3 data-field" id="formBasicPassword">
                                         <InputGroup.Text className='data-field-icon'
-                                                         id="basic-addon1"><FiLock/></InputGroup.Text>
+                                            id="basic-addon1"><FiLock /></InputGroup.Text>
                                         <Form.Control type="password" placeholder="Password" required
                                             // onChange={handleChange}
                                             // onBlur={handleBlur}
-                                                      aria-describedby="inputGroupPrepend"
-                                                      name="password"
-                                                      {...formik.getFieldProps("password")}
+                                            aria-describedby="inputGroupPrepend"
+                                            name="password"
+                                            {...formik.getFieldProps("password")}
                                         />
                                         {/*<Form.Control.Feedback type="invalid">*/}
                                         {/*    {errors.password}*/}
