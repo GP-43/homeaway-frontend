7ae403284ccc558c05cea2b643e1f4ffb17fd820
awanthairushan <2019is067@stu.ucsc.cmb.ac.lk>
2022-10-26 04:44:32 +0530

    created get date in frontend

--- src/components/user/place_description/make_booking/DatePickers.jsx
+++ src/components/user/place_description/make_booking/DatePickers.jsx
@@ -1,11 +1,17 @@
-import React, {useState} from "react";
-import {Row,Col} from "react-bootstrap";
+import React, {useEffect, useState} from "react";
+import {Row, Col} from "react-bootstrap";
 import {BsFillCalendarEventFill} from 'react-icons/bs';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
-function DatePickers() {
-    const [startDate, setStartDate] = useState(new Date());
+function DatePickers(props) {
+    const [date, setDate] = useState(new Date());
+
+    useEffect(() => {
+        if(props.endDate) {
+            setDate(props.endDate)
+        }
+    },[props.endDate])
 
     return (
         <Row className="date-card bg-white p-1">
@@ -17,8 +23,11 @@ function DatePickers() {
             <Col className="date-datepicker-col px-0 me-2 bg-white">
                 <DatePicker
                     className="datePicker m-2 bg-white"
-                    selected={startDate}
-                    onChange={(date) => setStartDate(date)}
+                    selected={date}
+                    onChange={(date)=>{
+                        props.handleOnDateDetails(new Date(date))
+                        setDate(new Date(date))
+                    }}
                 />
             </Col>
         </Row>
--- src/components/user/place_description/make_booking/MakeBooking.jsx
+++ src/components/user/place_description/make_booking/MakeBooking.jsx
@@ -1,49 +1,63 @@
-import React, { useState } from "react";
-import { Row, Col, Button } from "react-bootstrap";
+import React, {useEffect, useState} from "react";
+import {Row, Col, Button} from "react-bootstrap";
 import DatePickers from "./DatePickers";
 import TimePickers from "./TimePickers";
 import StripeCheckout from "react-stripe-checkout";
 
 function MakeBooking() {
-  function handleToken(token) {}
+    function handleToken(token) {
+    }
 
-  return (
-    <Row className="make-booking-card1 mx-5 px-3 mt-2 d-flex">
-      <Col className="booking-col" xs lg="2">
-        <div className="d-flex m-1 pb-1">
-          <b className="text">Date:</b>
-          <DatePickers className="date-component" value="" />
-        </div>
-        <div className="d-flex m-1 pb-1">
-          <b className="text-time">Time:</b>
-          <TimePickers />
-        </div>
-        <div className="mx-5 ps-4 mt-4">
-          <h1 className="text-rent-fee">
-            <b>LKR 12,500.00</b>
-          </h1>
-        </div>
-      </Col>
-      <Col className="booking-col" xs lg="2">
-        <div className="d-flex m-1 pb-1">
-          <b className="text">to:</b>
-          <DatePickers className="date-component" value="" />
-        </div>
-        <div className="d-flex m-1 pb-1">
-          <b className="text-time">to:</b>
-          <TimePickers />
-        </div>
-        <div className="ms-5 ps-5 mt-4">
-          {/* <Button >Pay Now</Button> */}
-          <StripeCheckout
-            className="dark button-pay-now btn-lg"
-            stripeKey="pk_test_51LfqFUSIxyJCNgPZYTW7Jx2LYdeiO8R1R3Bv9Ks6U93UiEPBwWTW5Bf634oubEFbFt615IvxBWnUiUVLNwsHnsPP000Mh2B2BX"
-            token={handleToken}
-          />
-        </div>
-      </Col>
-    </Row>
-  );
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
+
+    const handleOnStartDate = (date) => {
+        setStartDate(date);
+        setEndDate(date);
+    }
+    const handleOnEndDate = (date) => {
+        setEndDate(date);
+    }
+
+    return (
+        <Row className="make-booking-card1 mx-5 px-3 mt-2 d-flex">
+            <Col className="booking-col" xs lg="2">
+                <div className="d-flex m-1 pb-1">
+                    <b className="text">Date:</b>
+                    <DatePickers className="date-component" findClickedDatePicker={1}
+                                 handleOnDateDetails={handleOnStartDate}/>
+                </div>
+                <div className="d-flex m-1 pb-1">
+                    <b className="text-time">Time:</b>
+                    <TimePickers/>
+                </div>
+                <div className="mx-5 ps-4 mt-4">
+                    <h1 className="text-rent-fee">
+                        <b>LKR 12,500.00</b>
+                    </h1>
+                </div>
+            </Col>
+            <Col className="booking-col" xs lg="2">
+                <div className="d-flex m-1 pb-1">
+                    <b className="text">to:</b>
+                    <DatePickers className="date-component" findClickedDatePicker={2}
+                                 handleOnDateDetails={handleOnEndDate} endDate={endDate}/>
+                </div>
+                <div className="d-flex m-1 pb-1">
+                    <b className="text-time">to:</b>
+                    <TimePickers/>
+                </div>
+                <div className="ms-5 ps-5 mt-4">
+                    {/* <Button >Pay Now</Button> */}
+                    <StripeCheckout
+                        className="dark button-pay-now btn-lg"
+                        stripeKey="pk_test_51LfqFUSIxyJCNgPZYTW7Jx2LYdeiO8R1R3Bv9Ks6U93UiEPBwWTW5Bf634oubEFbFt615IvxBWnUiUVLNwsHnsPP000Mh2B2BX"
+                        token={handleToken}
+                    />
+                </div>
+            </Col>
+        </Row>
+    );
 }
 
 export default MakeBooking;
