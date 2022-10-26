0ffdf68fdd6c4a7dccc496fcce64554df38dde34
Thushan Dananjana <62166556+Thushan98@users.noreply.github.com>
2022-10-26 11:29:28 +0530

    mobile validate

--- src/views/pages/anons/Signup.jsx
+++ src/views/pages/anons/Signup.jsx
@@ -9,7 +9,7 @@ import { Formik, ErrorMessage } from 'formik';
 
 function Signup() {
 
-    const phoneRegExp = /\+[0-9]{1,4}-[0-9]{9}/;
+    const phoneRegExp = /^(?:0|94|\+94)(11|21|23|24|25|26|27|31|32|33|34|35|36|37|38|41|45|47|51|52|54|55|57|63|65|66|67|81|91|70|71|72|73|74|75|76|77|78|79)?\d{7}/;
 
     const navigate = useNavigate();
 
