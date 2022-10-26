fe20466ea80113bcbe9fbd5e198f9bfd7027f5db
Tharaki-99 <dilshanitharaki@gmail.com>
2022-10-26 13:12:29 +0530

    done

--- src/components/admin/earnings/Earning.jsx
+++ src/components/admin/earnings/Earning.jsx
@@ -1,23 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import { Row, Col } from 'react-bootstrap';
-import ProgressBar from './ProgressBar';
 import { FaCircle } from "react-icons/fa";
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import axios from 'axios';
 const base_url = process.env.REACT_APP_BASE_URL;
 
 function Earning() {
-    const percentage = 66;
+    const [incomeDetails, setIncomeDetails] = useState({});
+    const [totalIncome, setIncome] = useState();
+    useEffect((event) => {
+        axios
+            .get(`${base_url}/admin/totalIncome`)
+            .then((data) => {
+                const incomeDetails = data.data;
+                setIncomeDetails({ ...incomeDetails });
+                setIncome(incomeDetails[0]?.total_profit)
+                console.log(totalIncome);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
 
     const [details, setDetails] = useState({});
     const [count, setCount] = useState();
     useEffect((event) => {
         axios
-            .get(`${base_url}/admin/totalIncome`)
+            .get(`${base_url}/admin/totalPayment`)
             .then((data) => {
                 const details = data.data;
                 setDetails({ ...details });
-                setCount(details[0]?.total_profit)
+                setCount(details[0]?.total_income)
                 console.log(count);
             })
             .catch((error) => {
@@ -25,27 +38,26 @@ function Earning() {
             });
     }, []);
 
+    const percentage = (count - totalIncome) * 100 / count;
+
     return (
         <Col className='transaction'>
-            <Row className='mx-0 search-part mt-4 pt-4 '>
+            <Row className='mx-0 search-part mt-4 pt-3 '>
                 <Col className='ps-4 transaction-heading'>
                     <Row>
                         Earnings
                     </Row>
                     <Row>
                         <Col className='earn ps-0 ms-0'>
-                            Rs. {count}
+                            Rs. {totalIncome}
                         </Col>
                     </Row>
-                    <Row className='py-1 px-0'>
-                        <ProgressBar progress='60' />
-                    </Row>
-                    <Row className='week-heading'>
+                    <Row className='pt-4 week-heading'>
                         Summary
                     </Row>
                     <Row>
                         <Col className='content-progress'>
-                            <Row className='py-3'>
+                            <Row className='py-2'>
                                 <Col className='other-circle' xs={1}>
                                     <FaCircle />
                                 </Col>
@@ -53,7 +65,7 @@ function Earning() {
                                     Total Transaction Amount
                                 </Col>
                                 <Col xs={2}>
-                                    67%
+                                    {percentage}%
                                 </Col>
 
                             </Row>
@@ -65,13 +77,13 @@ function Earning() {
                                     Our Amount
                                 </Col>
                                 <Col xs={2}>
-                                    33%
+                                    {100 - percentage}%
                                 </Col>
 
                             </Row>
                         </Col>
                         <Col xs={4}>
-                            <CircularProgressbar className='p-3 cicle-graph' value={percentage} styles={buildStyles({ pathColor: "#0EFA5E", trailColor: "#DFFF17" })} />
+                            <CircularProgressbar className='px-3 pt-1 cicle-graph' value={percentage} styles={buildStyles({ pathColor: "#0EFA5E", trailColor: "#DFFF17" })} />
                         </Col>
                     </Row>
 
--- src/components/admin/earnings/TodayEarning.jsx
+++ src/components/admin/earnings/TodayEarning.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Row, Col } from 'react-bootstrap';
+import axois from "axios";
+const base_url = process.env.REACT_APP_BASE_URL;
 
 function TodayEarning() {
 
     const current = new Date();
     const date = `${current.getDate()}/${current.getMonth() + 1}/${current.getFullYear()}`;
+
+    const [details, setDetails] = useState({});
+    const [count, setCount] = useState();
+    useEffect((event) => {
+        axois
+            .get(`${base_url}/admin/todayEarnings`)
+            .then((data) => {
+                const details = data.data;
+                setDetails({ ...details });
+                setCount(details[0]?.today_income)
+                console.log("earning", count);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
 
     return (
         <Col className='earnings mt-4'>
@@ -16,7 +34,7 @@ function TodayEarning() {
                     {date}
                 </Row>
                 <Row className='pt-3 amount'>
-                    Rs.15,000
+                    {count}
                 </Row>
             </Row>
 
